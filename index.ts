773e4529a20fc3e0751e5996a10c53255e5d9b19
seveibar <seveibar@gmail.com>
2024-08-24 08:55:15 -0700

    publish first version set up publishing and testing workflow

--- index.ts
+++ index.ts
@@ -1,3 +1,1 @@
-export { Board } from "./lib/components/normal-components/Board"
-export { Project } from "./lib/Project"
-export { Resistor } from "./lib/components/normal-components/Resistor"
+export * from "./lib/index"
