7c6d58d13f3f75fd7e8a9e7d6aa92573ab4a4c4a
seveibar <seveibar@gmail.com>
2024-08-25 12:56:51 -0700

    bootstrap chip component

---
+++ lib/components/normal-components/Chip.ts
@@ -0,0 +1,4 @@
+import { NormalComponent } from "lib/components/base-components/NormalComponent"
+import { chipProps } from "@tscircuit/props"
+
+export class Chip extends NormalComponent<typeof chipProps> {}
