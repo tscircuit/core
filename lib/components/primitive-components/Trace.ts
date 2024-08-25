7f49c95eb2feff42b04d61da7c223572009c1e1d
seveibar <seveibar@gmail.com>
2024-08-25 00:01:28 -0700

    tracehints working

--- lib/components/primitive-components/Trace.ts
+++ lib/components/primitive-components/Trace.ts
@@ -1,7 +1,12 @@
 import { traceProps } from "@tscircuit/props"
 import { PrimitiveComponent } from "../base-components/PrimitiveComponent"
 import type { Port } from "./Port"
-import { IJumpAutorouter, autoroute } from "@tscircuit/infgrid-ijump-astar"
+import {
+  IJumpAutorouter,
+  autoroute,
+  getObstaclesFromSoup,
+  markObstaclesAsConnected,
+} from "@tscircuit/infgrid-ijump-astar"
 import type {
   AnySoupElement,
   PCBTrace,
@@ -17,6 +22,20 @@ import { computeObstacleBounds } from "lib/utils/autorouting/computeObstacleBoun
 import { projectPointInDirection } from "lib/utils/projectPointInDirection"
 import type { TraceHint } from "./TraceHint"
 import { findPossibleTraceLayerCombinations } from "lib/utils/autorouting/findPossibleTraceLayerCombinations"
+import { pairs } from "lib/utils/pairs"
+import { mergeRoutes } from "lib/utils/autorouting/mergeRoutes"
+
+type PcbRouteObjective =
+  | RouteHintPoint
+  | { layers: string[]; x: number; y: number; via?: boolean }
+
+const portToObjective = (port: Port): PcbRouteObjective => {
+  const portPosition = port.getGlobalPcbPosition()
+  return {
+    ...portPosition,
+    layers: port.getAvailablePcbLayers(),
+  }
+}
 
 export class Trace extends PrimitiveComponent<typeof traceProps> {
   source_trace_id: string | null = null
@@ -162,16 +181,89 @@ export class Trace extends PrimitiveComponent<typeof traceProps> {
     // When we have hints, we have to order the hints then route between each
     // terminal of the trace and the hints
     // TODO order based on proximity to ports
-    const orderedHintsAndPorts: Array<RouteHintPoint | { layers: string[] }> = [
-      { layers: ports[0].port.getAvailablePcbLayers() },
+    const orderedRouteObjectives: PcbRouteObjective[] = [
+      portToObjective(ports[0].port),
       ...pcbRouteHints,
-      { layers: ports[1].port.getAvailablePcbLayers() },
+      portToObjective(ports[1].port),
     ]
 
-    const candidateLayerCombinations =
-      findPossibleTraceLayerCombinations(orderedHintsAndPorts)
+    // Hints can indicate where there should be a via, but the layer is allowed
+    // to be unspecified, therefore we need to find possible layer combinations
+    // to go to each hint and still route to the start and end points
+    const candidateLayerCombinations = findPossibleTraceLayerCombinations(
+      orderedRouteObjectives,
+    )
 
-    console.log({ candidateLayerCombinations })
+    if (candidateLayerCombinations.length === 0) {
+      this.renderError(
+        `Could not find a common layer (using hints) for trace ${this.getString()}`,
+      )
+    }
+
+    // Cache the PCB obstacles, they'll be needed for each segment between
+    // ports/hints
+    const obstacles = getObstaclesFromSoup(this.project!.db.toArray())
+    markObstaclesAsConnected(
+      obstacles,
+      orderedRouteObjectives,
+      this.source_trace_id!,
+    )
+
+    // TODO explore all candidate layer combinations if one fails
+    const candidateLayerSelections = candidateLayerCombinations[0].layer_path
+
+    /**
+     * Apply the candidate layer selections to the route objectives, now we
+     * have a set of points that have definite layers
+     */
+    const orderedRoutePoints = orderedRouteObjectives.map((t, idx) => {
+      if (t.via) {
+        return {
+          ...t,
+          via_to_layer: candidateLayerSelections[idx],
+        }
+      }
+      return { ...t, layers: [candidateLayerSelections[idx]] }
+    })
+
+    const routes: PCBTrace["route"][] = []
+    for (const [a, b] of pairs(orderedRoutePoints)) {
+      const BOUNDS_MARGIN = 2 //mm
+      const ijump = new IJumpAutorouter({
+        input: {
+          obstacles,
+          connections: [
+            {
+              name: this.source_trace_id!,
+              pointsToConnect: [a, b],
+            },
+          ],
+          layerCount: 1,
+          bounds: {
+            minX: Math.min(a.x, b.x) - BOUNDS_MARGIN,
+            maxX: Math.max(a.x, b.x) + BOUNDS_MARGIN,
+            minY: Math.min(a.y, b.y) - BOUNDS_MARGIN,
+            maxY: Math.max(a.y, b.y) + BOUNDS_MARGIN,
+          },
+        },
+      })
+      const traces = ijump.solveAndMapToTraces()
+      if (traces.length === 0) {
+        this.renderError(
+          `Could not find a route between ${a.x}, ${a.y} and ${b.x}, ${b.y}`,
+        )
+        return
+      }
+      // TODO ijump returns multiple traces for some reason
+      const [trace] = traces as PCBTrace[]
+      routes.push(trace.route)
+    }
+
+    const pcb_trace = db.pcb_trace.insert({
+      route: mergeRoutes(routes),
+      source_trace_id: this.source_trace_id!,
+    })
+    this.pcb_trace_id = pcb_trace.pcb_trace_id
   }
 
   doInitialSchematicTraceRender(): void {
