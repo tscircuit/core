bd56ed20b2c42faa97706e797b8e1e7d62a9af14
seveibar <seveibar@gmail.com>
2024-08-28 20:53:59 -0700

    register bug in catalogue

--- lib/register-catalogue.ts
+++ lib/register-catalogue.ts
@@ -1,3 +1,8 @@
 import * as Components from "./components"
 import { extendCatalogue } from "./fiber/catalogue"
 extendCatalogue(Components)
+
+// Aliases
+extendCatalogue({
+  Bug: Components.Chip,
+})
