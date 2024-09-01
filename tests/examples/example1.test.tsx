7cf4cedd7311d942cd245a6240052fa94167ca60
seveibar <seveibar@gmail.com>
2024-09-01 11:01:01 -0700

    fix trace overlapping

--- tests/examples/example1.test.tsx
+++ tests/examples/example1.test.tsx
@@ -9,6 +9,8 @@ test("example1", async () => {
         name="U1"
         footprint="soic8"
         pinLabels={{ pin1: "PWR", pin8: "GND" }}
+        pcbX={0}
+        pcbY={0}
       />
       <resistor
         name="R1"
@@ -16,6 +18,9 @@ test("example1", async () => {
         resistance="10k"
         pullupFor=".U1 port.2"
         pullupTo="net.5v"
+        pcbX={4}
+        pcbY={0}
+        pcbRotation={90}
       />
       <capacitor
         name="C1"
@@ -23,13 +28,22 @@ test("example1", async () => {
         footprint="0603"
         decouplingFor=".U1 port.PWR"
         decouplingTo="net.GND"
+        pcbX={-5}
+        pcbY={0}
+        pcbRotation={90}
       />
-      <jumper name="J1" footprint="pinrow4" />
+      <jumper
+        pcbRotation={90}
+        pcbX={0}
+        pcbY={-4}
+        name="J1"
+        footprint="pinrow4"
+      />
 
-      {/* <trace from=".J1 pin.1" to=".U1 .PWR" />
+      <trace from=".J1 pin.1" to=".U1 .PWR" />
       <trace from=".J1 pin.2" to=".U1 port.2" />
       <trace from=".J1 pin.3" to=".U1 port.3" />
-      <trace from=".J1 pin.4" to=".U1 .GND" /> */}
+      <trace from=".J1 pin.4" to=".U1 .GND" />
     </board>,
   )
 
