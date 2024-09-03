e2f0d59c86ea0ec3397dd257236b0e73fcc17771
seveibar <seveibar@gmail.com>
2024-09-03 15:57:29 -0700

    fix group not rendering react subtrees

--- lib/components/primitive-components/Group.ts
+++ lib/components/primitive-components/Group.ts
@@ -2,9 +2,11 @@ import { groupProps } from "@tscircuit/props"
 import { PrimitiveComponent } from "../base-components/PrimitiveComponent"
 import { compose, identity } from "transformation-matrix"
 import { z } from "zod"
+import { NormalComponent } from "../base-components/NormalComponent"
+
 export class Group<
   Props extends z.ZodType<any, any, any> = typeof groupProps,
-> extends PrimitiveComponent<Props> {
+> extends NormalComponent<Props> {
   get config() {
     return {
       zodProps: groupProps as unknown as Props,
