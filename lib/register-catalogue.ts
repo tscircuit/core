91f83ef5906144c8edcd78a69101ef0cc0e93c50
Abhijit Roy <roya51788@gmail.com>
2024-09-08 23:10:28 +0530

    fix:typo

--- lib/register-catalogue.ts
+++ lib/register-catalogue.ts
@@ -1,12 +1,10 @@
 import * as Components from "./components"
 import { extendCatalogue } from "./fiber/catalogue"
-import { SilkscreenPath } from "./components/primitive-components/SilkscreenPath"
 
 // Register all components, generally you don't need to manually
 // register a component, as long as it's exported from lib/components
 // it'll automatically be registered!
 extendCatalogue(Components)
-
 
 // Aliases (only when class name is different than the name of the component)
 extendCatalogue({
