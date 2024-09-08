263ac098dff9b2e106242c44d6213ad1d579aabd
Abhijit Roy <roya51788@gmail.com>
2024-09-08 22:42:14 +0530

    fix: Unsupported silkscreenpath

--- lib/register-catalogue.ts
+++ lib/register-catalogue.ts
@@ -1,10 +1,15 @@
 import * as Components from "./components"
 import { extendCatalogue } from "./fiber/catalogue"
+import { SilkscreenPath } from "./components/primitive-components/SilkscreenPath"
 
 // Register all components, generally you don't need to manually
 // register a component, as long as it's exported from lib/components
 // it'll automatically be registered!
 extendCatalogue(Components)
+extendCatalogue({
+  ...Components,
+  SilkscreenPath,
+})
 
 // Aliases (only when class name is different than the name of the component)
 extendCatalogue({
