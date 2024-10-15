7210b57d3a026cac860cbd365243dc2c6277da58
Rishabh Gupta <belikerishabh@gmail.com>
2024-10-15 23:49:30 +0530

    test: fix

--- tests/components/normal-components/power-source.test.tsx
+++ tests/components/normal-components/power-source.test.tsx
@@ -6,13 +6,11 @@ it("should render a power source", async () => {
 
   project.add(
     <board width="10mm" height="10mm">
-      {/* <powersource name="pwr" voltage={5} schX={2} schY={3} pcbX={0} pcbY={0} /> */}
-      <netalias net="net1" />
+      <powersource name="pwr" voltage={5} schX={2} schY={3} pcbX={0} pcbY={0} />
     </board>
   );
 
   project.render();
-  console.log(project.getCircuitJson());
   expect(project.db.schematic_component.list()).toHaveLength(1);
   expect(project.db.schematic_port.list()).toHaveLength(2);
 });
