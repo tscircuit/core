244732decab6c7c39fd95484e76815d0372317b1
Rishabh Gupta <belikerishabh@gmail.com>
2024-10-16 02:08:55 +0530

    fix: schX and schY

--- lib/components/primitive-components/NetAlias.ts
+++ lib/components/primitive-components/NetAlias.ts
@@ -1,9 +1,6 @@
-import { z } from "zod"
+import { netAliasProps } from "@tscircuit/props"
 import { PrimitiveComponent } from "../base-components/PrimitiveComponent"
-
-export const netAliasProps = z.object({
-  net: z.string(),
-})
+import { Port } from "./Port"
 
 export class NetAlias extends PrimitiveComponent<typeof netAliasProps> {
   source_net_alias_id?: string
@@ -15,14 +12,24 @@ export class NetAlias extends PrimitiveComponent<typeof netAliasProps> {
     }
   }
 
+  initPorts() {
+    this.add(
+      new Port({
+        name: "pin1",
+        pinNumber: 1,
+        aliases: ["anode", "pos", "left"],
+      }),
+    )
+  }
+
   doInitialSchematicComponentRender(): void {
     const { db } = this.root!
     const { _parsedProps: props } = this
 
     const netAlias = db.schematic_net_label.insert({
-      text: props.net,
-      source_net_id: props.net,
-      center: { x: 0, y: 0 },
+      text: props.net!,
+      source_net_id: props.net!,
+      center: { x: props.schX ?? 0, y: props.schY ?? 0 },
       anchor_side: "bottom",
     })
 
