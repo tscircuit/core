b6ab054a5aed6fe7c8f90b2955ad80e1974fae64
Your Name <you@example.com>
2024-10-24 07:46:16 +0200

    Implemented <inductor/> component

---
+++ tests/components/normal-components/inductor.test.tsx
@@ -0,0 +1,17 @@
+import { test, expect } from "bun:test"
+import { getTestFixture } from "tests/fixtures/get-test-fixture"
+
+test("<inductor /> component", async () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <board width="12mm" height="10mm">
+      <inductor name="U1" inductance="10" footprint="axial_p0.3in" pcbX={0} pcbY={0} />
+    </board>,
+  )
+
+  circuit.render()
+
+  expect(circuit).toMatchPcbSnapshot(import.meta.path)
+  expect(circuit).toMatchSchematicSnapshot(import.meta.path)
+})
