3cfa4176a89089c829173bc7d4b0082b18df9dd5
seveibar <seveibar@gmail.com>
2024-11-04 16:58:48 -0800

    remove log line

--- lib/components/base-components/NormalComponent.ts
+++ lib/components/base-components/NormalComponent.ts
@@ -326,7 +326,6 @@ export class NormalComponent<
       primaryPortLabels[port] = Array.isArray(label) ? label[0] : label
     }
 
-    console.log("inserting schematic component")
     const schematic_component = db.schematic_component.insert({
       center: { x: props.schX ?? 0, y: props.schY ?? 0 },
       rotation: props.schRotation ?? 0,
