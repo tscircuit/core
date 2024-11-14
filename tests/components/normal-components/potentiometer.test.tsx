a4d5bb3c706b0fa48833005f15a5e883081353cc
tscircuitbot <tscircuitbot@users.noreply.github.com>
2024-11-14 04:09:20 +0000

    formatbot: Automatically format code

--- tests/components/normal-components/potentiometer.test.tsx
+++ tests/components/normal-components/potentiometer.test.tsx
@@ -6,10 +6,34 @@ test("potentiometer schematic", () => {
 
   circuit.add(
     <board width="10mm" height="10mm">
-      <potentiometer name="P1" maxResistance="10k" schX={0} schY={0} schRotation={0} />
-      <potentiometer name="P2" maxResistance="10k" schX={2} schY={0} schRotation={90} />
-      <potentiometer name="P3" maxResistance="10k" schX={4} schY={0} schRotation={180} />
-      <potentiometer name="P4" maxResistance="10k" schX={6} schY={0} schRotation={270} />
+      <potentiometer
+        name="P1"
+        maxResistance="10k"
+        schX={0}
+        schY={0}
+        schRotation={0}
+      />
+      <potentiometer
+        name="P2"
+        maxResistance="10k"
+        schX={2}
+        schY={0}
+        schRotation={90}
+      />
+      <potentiometer
+        name="P3"
+        maxResistance="10k"
+        schX={4}
+        schY={0}
+        schRotation={180}
+      />
+      <potentiometer
+        name="P4"
+        maxResistance="10k"
+        schX={6}
+        schY={0}
+        schRotation={270}
+      />
     </board>,
   )
 
