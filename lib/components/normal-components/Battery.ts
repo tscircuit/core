4e7a01d125c7fbcaccc90639330ccdbd5af53e04
Shibo <shibosoftwaredev@gmail.com>
2024-11-16 10:57:51 +0200

    fixed battery symbol not rendering (#314)
    * fixed battery symbol not rendering
    
    * removed lockfile

--- lib/components/normal-components/Battery.ts
+++ lib/components/normal-components/Battery.ts
@@ -22,20 +22,12 @@ export class Battery extends NormalComponent<
   }
 
   initPorts() {
-    this.add(
-      new Port({
-        name: "pin1",
-        pinNumber: 1,
-        aliases: ["anode", "pos", "left"],
-      }),
-    )
-    this.add(
-      new Port({
-        name: "pin2",
-        pinNumber: 2,
-        aliases: ["cathode", "neg", "right"],
-      }),
-    )
+    super.initPorts({
+      additionalAliases: {
+        pin1: ["anode", "pos", "left"],
+        pin2: ["cathode", "neg", "right"],
+      },
+    })
   }
 
   doInitialSourceRender() {
