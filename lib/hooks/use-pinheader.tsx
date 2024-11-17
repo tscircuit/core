9159a89e924978f45f7b9c5838af4b28c2f3ac7c
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-11-17 19:23:02 +0530

    performaing suggested changes

--- lib/hooks/use-pinheader.tsx
+++ lib/hooks/use-pinheader.tsx
@@ -1,8 +1,15 @@
-import type { PinHeaderProps } from "@tscircuit/props"
-import { createUseComponent } from "./create-use-component"
+import type { PinHeaderProps } from "@tscircuit/props";
+import { createUseComponent } from "./create-use-component";
 
-export const usePinHeader = (pinLabels: string[]) =>
+export const usePinHeader = (
+  name: string,
+  options: {
+    pinLabels: PinHeaderProps["pinLabels"];
+  }
+) =>
   createUseComponent(
-    (props: PinHeaderProps) => <pinheader pinLabels={pinLabels} {...props} />,
-    pinLabels,
-  )
+    (props: PinHeaderProps) => (
+      <pinheader {...props} name={name} pinLabels={options.pinLabels} />
+    ),
+    options.pinLabels!
+  );
