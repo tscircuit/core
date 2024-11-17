76e291dc74ca322ecc0271e00b2039c37ff4da20
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-11-17 19:25:34 +0530

    fix: fomrat fixes

--- lib/hooks/use-pinheader.tsx
+++ lib/hooks/use-pinheader.tsx
@@ -1,15 +1,15 @@
-import type { PinHeaderProps } from "@tscircuit/props";
-import { createUseComponent } from "./create-use-component";
+import type { PinHeaderProps } from "@tscircuit/props"
+import { createUseComponent } from "./create-use-component"
 
 export const usePinHeader = (
   name: string,
   options: {
-    pinLabels: PinHeaderProps["pinLabels"];
-  }
+    pinLabels: PinHeaderProps["pinLabels"]
+  },
 ) =>
   createUseComponent(
     (props: PinHeaderProps) => (
       <pinheader {...props} name={name} pinLabels={options.pinLabels} />
     ),
-    options.pinLabels!
-  );
+    options.pinLabels!,
+  )
