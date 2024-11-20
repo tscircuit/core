3f638caeaca88dc0fb15723c6124983b4e065516
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-11-20 22:31:01 +0530

    adding pinStyle test and snapshot

--- tests/utils/schematic/getAllDimensionsForSchematicBox.test.ts
+++ tests/utils/schematic/getAllDimensionsForSchematicBox.test.ts
@@ -1,7 +1,7 @@
-import { expect, test, describe } from "bun:test"
-import { getAllDimensionsForSchematicBox } from "lib/utils/schematic/getAllDimensionsForSchematicBox"
-import { getSchematicBoxSvg } from "./getSchematicBoxSvg"
-import "tests/fixtures/extend-expect-any-svg"
+import { expect, test, describe } from "bun:test";
+import { getAllDimensionsForSchematicBox } from "lib/utils/schematic/getAllDimensionsForSchematicBox";
+import { getSchematicBoxSvg } from "./getSchematicBoxSvg";
+import "tests/fixtures/extend-expect-any-svg";
 
 test("getAllDimensionsForSchematicBox 1", () => {
   const params: Parameters<typeof getAllDimensionsForSchematicBox>[0] = {
@@ -9,37 +9,34 @@ test("getAllDimensionsForSchematicBox 1", () => {
     schPinSpacing: 0.2,
     schPinStyle: {},
     pinCount: 8,
-  }
+  };
 
-  const dimensions = getAllDimensionsForSchematicBox(params)
+  const dimensions = getAllDimensionsForSchematicBox(params);
 
   expect(getSchematicBoxSvg(dimensions)).toMatchSvgSnapshot(
     import.meta.path,
-    "schematicbox1",
-  )
-})
+    "schematicbox1"
+  );
+});
 
 test("getAllDimensionsForSchematicBox 2 (pinStyle)", () => {
   const params: Parameters<typeof getAllDimensionsForSchematicBox>[0] = {
     schWidth: 1,
-    pinLabels: {
-      pin1: "VCC",
-    },
     schPinSpacing: 0.2,
     schPinStyle: {
-      VCC: { bottomMargin: 0.5 },
+      pin2: { bottomMargin: 0.5 },
       pin7: { topMargin: 0.5 },
     },
     pinCount: 8,
-  }
+  };
 
-  const dimensions = getAllDimensionsForSchematicBox(params)
+  const dimensions = getAllDimensionsForSchematicBox(params);
 
   expect(getSchematicBoxSvg(dimensions)).toMatchSvgSnapshot(
     import.meta.path,
-    "schematicbox2",
-  )
-})
+    "schematicbox2"
+  );
+});
 
 test("getAllDimensionsForSchematicBox 3 (4 sided with margins)", () => {
   const params: Parameters<typeof getAllDimensionsForSchematicBox>[0] = {
@@ -56,12 +53,35 @@ test("getAllDimensionsForSchematicBox 3 (4 sided with margins)", () => {
       bottomSize: 4,
     },
     pinCount: 16,
-  }
+  };
 
-  const dimensions = getAllDimensionsForSchematicBox(params)
+  const dimensions = getAllDimensionsForSchematicBox(params);
 
   expect(getSchematicBoxSvg(dimensions)).toMatchSvgSnapshot(
     import.meta.path,
-    "schematicbox3",
-  )
-})
+    "schematicbox3"
+  );
+});
+
+test("getAllDimensionsForSchematicBox 4 (schPinStyle accepting pinLabels)", () => {
+  const params: Parameters<typeof getAllDimensionsForSchematicBox>[0] = {
+    schWidth: 1,
+    schPinSpacing: 0.2,
+    pinLabels: {
+      pin1: "VCC",
+      pin8: "GND",
+    },
+    schPinStyle: {
+      VCC: { bottomMargin: 0.5 },
+      GND: { bottomMargin: 0.5 },
+    },
+    pinCount: 8,
+  };
+
+  const dimensions = getAllDimensionsForSchematicBox(params);
+
+  expect(getSchematicBoxSvg(dimensions)).toMatchSvgSnapshot(
+    import.meta.path,
+    "schematicbox4"
+  );
+});
