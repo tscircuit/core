58f1bdbd1db872a0f77c5acdd263736947e956e8
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2024-12-07 14:30:24 +0200

    fixing JSX type  module for React 19 types (#396)
    * fixing JSX type  module for React 19 types
    
    * fixing JSX type  module for React 19 types
    
    * lowercase

--- lib/fiber/intrinsic-jsx.ts
+++ lib/fiber/intrinsic-jsx.ts
@@ -1,53 +1,60 @@
 import type * as Props from "@tscircuit/props"
 
-declare global {
+export interface TscircuitElements {
+  resistor: Props.ResistorProps
+  capacitor: Props.CapacitorProps
+  inductor: Props.InductorProps
+  pushbutton: Props.PushButtonProps
+  diode: Props.DiodeProps
+  led: Props.LedProps
+  board: Props.BoardProps
+  jumper: Props.JumperProps
+  bug: Props.ChipProps
+  potentiometer: Props.PotentiometerProps
+  // TODO use ChipProps once it gets merged in @tscircuit/props
+  chip: Props.ChipProps
+  powersource: Props.PowerSourceProps
+  via: Props.ViaProps
+  schematicbox: Props.SchematicBoxProps
+  schematicline: Props.SchematicLineProps
+  schematicpath: Props.SchematicPathProps
+  schematictext: Props.SchematicTextProps
+  smtpad: Props.SmtPadProps
+  platedhole: Props.PlatedHoleProps
+  keepout: Props.PcbKeepoutProps
+  hole: Props.HoleProps
+  port: Props.PortProps
+  group: Props.GroupProps
+  netalias: Props.NetAliasProps
+  net: Props.NetProps
+  trace: Props.TraceProps
+  custom: any
+  component: Props.ComponentProps
+  footprint: any
+  silkscreentext: Props.SilkscreenTextProps
+  silkscreenpath: Props.SilkscreenPathProps
+  silkscreenline: Props.SilkscreenLineProps
+  silkscreenrect: Props.SilkscreenRectProps
+  silkscreencircle: Props.SilkscreenCircleProps
+  tracehint: Props.TraceHintProps
+  pcbtrace: Props.PcbTraceProps
+  fabricationnotetext: Props.FabricationNoteTextProps
+  fabricationnotepath: Props.FabricationNotePathProps
+  constraint: Props.ConstraintProps
+  constrainedlayout: Props.ConstrainedLayoutProps
+  battery: Props.BatteryProps
+  pinheader: Props.PinHeaderProps
+  subcircuit: Props.SubcircuitGroupProps
+  jscad: any
+}
+
+declare module "react" {
   namespace JSX {
-    interface IntrinsicElements {
-      resistor: Props.ResistorProps
-      capacitor: Props.CapacitorProps
-      inductor: Props.InductorProps
-      pushbutton: Props.PushButtonProps
-      diode: Props.DiodeProps
-      led: Props.LedProps
-      board: Props.BoardProps
-      jumper: Props.JumperProps
-      bug: Props.ChipProps
-      potentiometer: Props.PotentiometerProps
-      // TODO use ChipProps once it gets merged in @tscircuit/props
-      chip: Props.ChipProps
-      powersource: Props.PowerSourceProps
-      via: Props.ViaProps
-      schematicbox: Props.SchematicBoxProps
-      schematicline: Props.SchematicLineProps
-      schematicpath: Props.SchematicPathProps
-      schematictext: Props.SchematicTextProps
-      smtpad: Props.SmtPadProps
-      platedhole: Props.PlatedHoleProps
-      keepout: Props.PcbKeepoutProps
-      hole: Props.HoleProps
-      port: Props.PortProps
-      group: Props.GroupProps
-      netalias: Props.NetAliasProps
-      net: Props.NetProps
-      trace: Props.TraceProps
-      custom: any
-      component: Props.ComponentProps
-      footprint: any
-      silkscreentext: Props.SilkscreenTextProps
-      silkscreenpath: Props.SilkscreenPathProps
-      silkscreenline: Props.SilkscreenLineProps
-      silkscreenrect: Props.SilkscreenRectProps
-      silkscreencircle: Props.SilkscreenCircleProps
-      tracehint: Props.TraceHintProps
-      pcbtrace: Props.PcbTraceProps
-      fabricationnotetext: Props.FabricationNoteTextProps
-      fabricationnotepath: Props.FabricationNotePathProps
-      constraint: Props.ConstraintProps
-      constrainedlayout: Props.ConstrainedLayoutProps
-      battery: Props.BatteryProps
-      pinheader: Props.PinHeaderProps
-      subcircuit: Props.SubcircuitGroupProps
-      jscad: any
-    }
+    interface IntrinsicElements extends TscircuitElements {}
+  }
+}
+declare module "react/jsx-runtime" {
+  namespace JSX {
+    interface IntrinsicElements extends TscircuitElements {}
   }
 }
