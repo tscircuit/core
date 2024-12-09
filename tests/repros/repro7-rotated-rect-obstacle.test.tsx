127634fa954f94a1ac72ac59649fbf910b6f5628
Shibo <shibosoftwaredev@gmail.com>
2024-12-09 22:36:49 +0200

    rotated rect obstacle test

---
+++ tests/repros/repro7-rotated-rect-obstacle.test.tsx
@@ -0,0 +1,40 @@
+import { test, expect } from "bun:test"
+import { getTestFixture } from "tests/fixtures/get-test-fixture"
+
+test("repro7-rotated-rect-obstacle", async () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <board width="30mm" height="30mm">
+      <resistor
+        name="R1"
+        resistance="10k"
+        footprint="0402"
+        pcbX={-6}
+        pcbY={0}
+      />
+      <resistor name="R2" resistance="10k" footprint="0402" pcbX={6} pcbY={0} />
+      <trace from=".R1 > .pin2" to=".R2 > .pin1" />
+
+      <chip name='obstacle' pcbX={0} pcbY={0} pcbRotation={33} footprint='ms012' />
+
+
+      <resistor
+        name="R3"
+        resistance="10k"
+        footprint="0402"
+        pcbX={-6}
+        pcbY={-10}
+      />
+      <resistor name="R4" resistance="10k" footprint="0402" pcbX={6} pcbY={-10} />
+      <trace from=".R3 > .pin2" to=".R4 > .pin1" />
+
+      <chip name='obstacle' pcbX={0} pcbY={-10} pcbRotation={140} footprint='ms012' />
+    </board>,
+  )
+
+  circuit.render()
+
+  expect(circuit).toMatchPcbSnapshot(import.meta.path)
+})
+
