f30ad3de4a8e9c7c7d772f924cd34556c53fe3ac
Severin Ibarluzea <seve700@gmail.com>
2024-12-19 08:18:14 -0800

    rename eval error

--- lib/Circuit.ts
+++ lib/Circuit.ts
@@ -12,7 +12,7 @@ type RootCircuitEventName =
   | "renderable:renderLifecycle:anyEvent"
   | `renderable:renderLifecycle:${RenderPhase}:start`
   | `renderable:renderLifecycle:${RenderPhase}:end`
-  | "eval_error"
+  | "external:evalError"
 
 export class Circuit {
   firstChild: PrimitiveComponent | null = null
