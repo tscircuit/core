1dba6cacc7f30617242a5ee6658e72178be878a6
seveibar <seveibar@gmail.com>
2024-12-19 09:59:33 -0800

    minor refactoring of old "project" code, apply manual edits test

--- tests/fixtures/circuit-snapshot.test.tsx
+++ tests/fixtures/circuit-snapshot.test.tsx
@@ -2,19 +2,19 @@ import { it, expect } from "bun:test"
 import { getTestFixture } from "./get-test-fixture"
 
 it("should be able to snapshot a circuit", async () => {
-  const { project } = await getTestFixture()
+  const { circuit } = await getTestFixture()
 
-  project.add(
+  circuit.add(
     <board width="10mm" height="10mm">
       <resistor name="R1" resistance="10k" footprint="0402" />
     </board>,
   )
 
-  project.render()
+  circuit.render()
 
-  const pcb_smtpads = project.db.pcb_smtpad.list()
+  const pcb_smtpads = circuit.db.pcb_smtpad.list()
   expect(pcb_smtpads.every((smt) => !Number.isNaN(smt.x))).toBe(true)
   expect(pcb_smtpads.every((smt) => !Number.isNaN(smt.y))).toBe(true)
 
-  await expect(project.getSoup()).toMatchPcbSnapshot(import.meta.path)
+  await expect(circuit.getSoup()).toMatchPcbSnapshot(import.meta.path)
 })
