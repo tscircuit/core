22611e1e2dce65cfa63d865731cfdd9d588c16db
tscircuitbot <tscircuitbot@users.noreply.github.com>
2024-12-21 20:45:11 +0000

    formatbot: Automatically format code

--- lib/components/primitive-components/Group/Group.ts
+++ lib/components/primitive-components/Group/Group.ts
@@ -336,14 +336,12 @@ export class Group<Props extends z.ZodType<any, any, any> = typeof groupProps>
   }
 
   doInitialSchematicLayout(): void {
-    
     this._computeSchematicPositionBeforeLayout()
     // The schematic_components are rendered in our children
     if (!this.isSubcircuit) return
 
     const props = this._parsedProps as SubcircuitGroupProps
     if (!props.schAutoLayoutEnabled) return
-
 
     const { db } = this.root!
 
