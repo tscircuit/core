d6fbb644f963c156d07ba95d7f37448f1ca2a123
seveibar <seveibar@gmail.com>
2025-01-04 22:15:33 -0800

    remove old port file, start to boilerplate method

--- lib/components/primitive-components/Port/areAllPcbPrimitivesOverlapping.ts
+++ lib/components/primitive-components/Port/areAllPcbPrimitivesOverlapping.ts
@@ -3,5 +3,7 @@ import type { PrimitiveComponent } from "lib/components/base-components/Primitiv
 export const areAllPcbPrimitivesOverlapping = (
   pcbPrimitives: PrimitiveComponent[],
 ): boolean => {
+  if (pcbPrimitives.length <= 1) return true
+  return false
   // TODO
 }
