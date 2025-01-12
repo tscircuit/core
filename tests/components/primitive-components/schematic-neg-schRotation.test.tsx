749e3c8d86303708b56e6888c022e9a9c6f4209d
Anshgrover23 <ag5989670@gmail.com>
2025-01-12 22:56:05 +0530

    fix snapshots

--- tests/components/primitive-components/schematic-neg-schRotation.test.tsx
+++ tests/components/primitive-components/schematic-neg-schRotation.test.tsx
@@ -7,38 +7,38 @@ it("should render rotated versions of capacitors", async () => {
   circuit.add(
     <board width="20mm" height="20mm">
       <capacitor
-        name="C1"
+        name="C1_90"
         schY={0}
         schX={3}
-        capacitance="10k"
+        capacitance="10µF"
         schRotation={-90}
       />
 
       <capacitor
-        name="C2"
+        name="C2_180"
         schY={0}
         schX={6}
-        capacitance="10k"
+        capacitance="10µF"
         schRotation={-180}
       />
 
       <capacitor
-        name="C3"
+        name="C3_270"
         schY={0}
         schX={9}
-        capacitance="10k"
+        capacitance="10µF"
         schRotation={-270}
       />
 
       <capacitor
-        name="C4"
+        name="C4_360"
         schY={3}
         schX={3}
-        capacitance="10k"
+        capacitance="10µF"
         schRotation={-360}
       />
 
-      <capacitor name="C5" schY={3} schX={6} capacitance="10k" />
+      <capacitor name="C5" schY={3} schX={6} capacitance="10µF" />
     </board>,
   )
 
