3c4ccacd8e8da708b59432dbb5b4dcacf3e102c5
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-02-10 00:12:36 +0530

    test: repro subcircuit overlap traces issues (#601)

---
+++ tests/examples/example19-subcircuit-overlap-traces.test.tsx
@@ -0,0 +1,237 @@
+import { test, expect } from "bun:test"
+import { getTestFixture } from "tests/fixtures/get-test-fixture"
+import { su } from "@tscircuit/soup-util"
+
+test("If the subcircuit is routing disabled, it should not have traces from the autorouter but it does have", async () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <board width="10mm" height="10mm" autorouter="auto-cloud">
+      <group subcircuit routingDisabled>
+        <resistor footprint="0402" resistance={1000} name="R1" pcbX={-2} />
+        <resistor footprint="0402" resistance={1000} name="R2" pcbX={2} />
+        <trace from=".R1 .1" to=".R2 .1" />
+      </group>
+    </board>,
+  )
+
+  await circuit.renderUntilSettled()
+
+  const pcb_traces = su(circuit.getCircuitJson()).pcb_trace.list()
+  expect(pcb_traces).toMatchInlineSnapshot(`
+    [
+      {
+        "pcb_trace_id": "pcb_trace_Net-(R1_source_component_0-Pad1)",
+        "route": [
+          {
+            "layer": "top",
+            "route_type": "wire",
+            "width": 0.16,
+            "x": 1.5,
+            "y": 0,
+          },
+          {
+            "layer": "top",
+            "route_type": "wire",
+            "width": 0.16,
+            "x": 0.9483,
+            "y": 0,
+          },
+        ],
+        "source_trace_id": "source_trace_0",
+        "trace_length": 0.5517,
+        "type": "pcb_trace",
+      },
+      {
+        "pcb_trace_id": "pcb_trace_Net-(R1_source_component_0-Pad1)",
+        "route": [
+          {
+            "layer": "top",
+            "route_type": "wire",
+            "width": 0.16,
+            "x": 0.9483,
+            "y": 0,
+          },
+          {
+            "layer": "top",
+            "route_type": "wire",
+            "width": 0.16,
+            "x": 0.3966,
+            "y": -0.5517,
+          },
+          {
+            "layer": "top",
+            "route_type": "wire",
+            "width": 0.16,
+            "x": -1.9483,
+            "y": -0.5517,
+          },
+          {
+            "layer": "top",
+            "route_type": "wire",
+            "width": 0.16,
+            "x": -2.5,
+            "y": 0,
+          },
+        ],
+        "source_trace_id": "source_trace_0",
+        "trace_length": 3.9053,
+        "type": "pcb_trace",
+      },
+    ]
+  `)
+})
+
+test("It is having traces from both autorouter and freerouting (Not in the final circuit json)", async () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <board width="10mm" height="10mm" autorouter="auto-cloud">
+      <group subcircuit>
+        <resistor footprint="0402" resistance={1000} name="R1" pcbX={-2} />
+        <resistor footprint="0402" resistance={1000} name="R2" pcbX={2} />
+        <trace from=".R1 .1" to=".R2 .1" />
+      </group>
+    </board>,
+  )
+
+  await circuit.renderUntilSettled()
+
+  const pcb_traces = su(circuit.getCircuitJson()).pcb_trace.list()
+  expect(pcb_traces).toMatchInlineSnapshot(`
+      [
+        {
+          "pcb_trace_id": "pcb_trace_0",
+          "route": [
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "start_pcb_port_id": "pcb_port_0",
+              "width": 0.16,
+              "x": -2.5,
+              "y": 0,
+            },
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": -2.12,
+              "y": 0,
+            },
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": -2.12,
+              "y": 1.3,
+            },
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": 1.5,
+              "y": 1.3,
+            },
+            {
+              "end_pcb_port_id": "pcb_port_2",
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": 1.5,
+              "y": 0,
+            },
+          ],
+          "source_trace_id": "source_trace_0",
+          "trace_length": 6.6,
+          "type": "pcb_trace",
+        },
+        {
+          "pcb_trace_id": "pcb_trace_source_trace_0--Pad1_R1_source_component_0--Pad1_R2_source_component_1",
+          "route": [
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": -2.5,
+              "y": 0,
+            },
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": -2.12,
+              "y": 0,
+            },
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": -2.12,
+              "y": 1.3,
+            },
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": 1.5,
+              "y": 1.3,
+            },
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": 1.5,
+              "y": 0,
+            },
+          ],
+          "source_trace_id": "source_trace_0",
+          "trace_length": 6.6,
+          "type": "pcb_trace",
+        },
+        {
+          "pcb_trace_id": "pcb_trace_Net-(R1_source_component_0-Pad1)",
+          "route": [
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": -2.5,
+              "y": 0,
+            },
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": -2.12,
+              "y": 0,
+            },
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": -2.12,
+              "y": 1.3,
+            },
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": 1.5,
+              "y": 1.3,
+            },
+            {
+              "layer": "top",
+              "route_type": "wire",
+              "width": 0.16,
+              "x": 1.5,
+              "y": 0,
+            },
+          ],
+          "source_trace_id": "source_trace_0",
+          "trace_length": 6.6,
+          "type": "pcb_trace",
+        },
+      ]
+    `)
+  expect(circuit.getCircuitJson()).toMatchPcbSnapshot(import.meta.path)
+})
