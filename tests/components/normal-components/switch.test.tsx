ac01f950caa289cee5b5b4e5fc793e82c747c185
techmannih <manishchaudharyttt@gmail.com>
2025-02-12 22:38:51 +0530

    add snapshot

--- tests/components/normal-components/switch.test.tsx
+++ tests/components/normal-components/switch.test.tsx
@@ -5,7 +5,13 @@ it("should render an switch", async () => {
   const { circuit } = getTestFixture()
   circuit.add(
     <board width="10mm" height="10mm">
-      <switch name="switch" type="spst" />
+      <switch name="switch" type="spst" schX={2} schY={2}/>
+      <switch name="switch" spst schX={-2} schY={-2}/>
+      <switch name="switch" type="spdt" schX={0} schY={0}/>
+      <switch name="switch" schX={2} schY={-2}/>
+      <switch name="switch" dpdt schX={-2} schY={2}/>
+      <switch name="switch" type="spdt" schX={0} schY={0}/>
+
     </board>,
   )
   circuit.render()
