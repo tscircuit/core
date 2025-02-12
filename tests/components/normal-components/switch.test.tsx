d2d2497c594007d1be000615c15cb26d911b72e1
techmannih <manishchaudharyttt@gmail.com>
2025-02-12 22:40:41 +0530

    format code

--- tests/components/normal-components/switch.test.tsx
+++ tests/components/normal-components/switch.test.tsx
@@ -5,13 +5,12 @@ it("should render an switch", async () => {
   const { circuit } = getTestFixture()
   circuit.add(
     <board width="10mm" height="10mm">
-      <switch name="switch" type="spst" schX={2} schY={2}/>
-      <switch name="switch" spst schX={-2} schY={-2}/>
-      <switch name="switch" type="spdt" schX={0} schY={0}/>
-      <switch name="switch" schX={2} schY={-2}/>
-      <switch name="switch" dpdt schX={-2} schY={2}/>
-      <switch name="switch" type="spdt" schX={0} schY={0}/>
-
+      <switch name="switch" type="spst" schX={2} schY={2} />
+      <switch name="switch" spst schX={-2} schY={-2} />
+      <switch name="switch" type="spdt" schX={0} schY={0} />
+      <switch name="switch" schX={2} schY={-2} />
+      <switch name="switch" dpdt schX={-2} schY={2} />
+      <switch name="switch" type="spdt" schX={0} schY={0} />
     </board>,
   )
   circuit.render()
