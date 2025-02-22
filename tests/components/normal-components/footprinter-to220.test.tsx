cd1ed2ecbe4b78d6ae1ad2d44b7bbada3353f6f6
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-02-23 01:08:02 +0530

    footprint added as peer deps (#643)
    * footprint added as peer deps
    
    * formatbot: Automatically format code
    
    * Update package.json
    
    Co-authored-by: Severin Ibarluzea <seve700@gmail.com>
    
    ---------
    
    Co-authored-by: tscircuitbot <tscircuitbot@users.noreply.github.com>
    Co-authored-by: Severin Ibarluzea <seve700@gmail.com>

---
+++ tests/components/normal-components/footprinter-to220.test.tsx
@@ -0,0 +1,19 @@
+import { expect, test } from "bun:test"
+import { getTestFixture } from "tests/fixtures/get-test-fixture"
+
+test("footprinter: to220 with 3 holes", () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <board width="30mm" height="10mm">
+      <chip footprint="to220_3" name="U3" />
+    </board>,
+  )
+
+  circuit.render()
+
+  const pcb_plated_holes = circuit.db.pcb_plated_hole.list()
+  expect(pcb_plated_holes).toHaveLength(3)
+
+  expect(circuit).toMatchPcbSnapshot(import.meta.path)
+})
