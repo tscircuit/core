e2b64f9bc2685e44c1364d60fc858cf71f226718
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-02-23 22:42:07 +0530

    chore: footprint and update jumper test with rows (#646)
    * chore: footprint and update jumper test with rows
    
    * update pinrow footprint snapshot
    
    * Update peer dev

--- tests/components/normal-components/jumper.test.tsx
+++ tests/components/normal-components/jumper.test.tsx
@@ -22,3 +22,24 @@ it("should render a jumper with pinrow4 footprint", async () => {
   expect(circuit).toMatchPcbSnapshot(import.meta.path)
   expect(circuit).toMatchSchematicSnapshot(import.meta.path)
 })
+
+it("It should render a jumper with the pinrow4 footprint and rows", async () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <board width="10mm" height="10mm">
+      <jumper
+        name="J1"
+        footprint="pinrow6_female_rows2"
+        pcbX={-2}
+        pcbY={-2}
+        schX={0}
+        schY={0}
+      />
+    </board>,
+  )
+
+  circuit.render()
+  expect(circuit).toMatchPcbSnapshot(`${import.meta.path}rows`)
+  expect(circuit).toMatchSchematicSnapshot(`${import.meta.path}rows`)
+})
