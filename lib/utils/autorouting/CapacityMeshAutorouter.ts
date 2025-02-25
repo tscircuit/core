1c8fb56ba026d27f47ad17aa997976e3a4e4741b
seveibar <seveibar@gmail.com>
2025-02-25 10:33:55 -0800

    wip

--- lib/utils/autorouting/CapacityMeshAutorouter.ts
+++ lib/utils/autorouting/CapacityMeshAutorouter.ts
@@ -1,3 +1,32 @@
 import { CapacityMeshSolver } from "@tscircuit/capacity-autorouter"
+import type { SimpleRouteJson, SimplifiedPcbTrace } from "./SimpleRouteJson"
+import type {
+  AutorouterCompleteEvent,
+  AutorouterErrorEvent,
+  AutorouterProgressEvent,
+  GenericLocalAutorouter,
+} from "./GenericLocalAutorouter"
 
-export 
+export class CapacityMeshAutorouter implements GenericLocalAutorouter {
+  input: SimpleRouteJson
+  isRouting = false
+
+  constructor(input: SimpleRouteJson) {
+    this.input = input
+  }
+  start(): void {
+    throw new Error("Method not implemented.")
+  }
+  stop(): void {
+    throw new Error("Method not implemented.")
+  }
+  on(event: "complete", callback: (ev: AutorouterCompleteEvent) => void): void
+  on(event: "error", callback: (ev: AutorouterErrorEvent) => void): void
+  on(event: "progress", callback: (ev: AutorouterProgressEvent) => void): void
+  on(event: unknown, callback: unknown): void {
+    throw new Error("Method not implemented.")
+  }
+  solveSync(): SimplifiedPcbTrace[] {
+    throw new Error("Method not implemented.")
+  }
+}
