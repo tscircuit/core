ac5040d70e4ff0fd627c7c3ac634c01f1b1b29c4
“mustafa” <mustafamulla765@gmail.com>
2025-02-27 13:38:45 +0530

    Exported simpleRouteJson for https://github.com/tscircuit/circuitjson.com/issues/10

--- lib/index.ts
+++ lib/index.ts
@@ -9,6 +9,7 @@ export * from "./hooks/use-led"
 export * from "./hooks/use-resistor"
 export * from "./utils/public-exports"
 export * from "./sel"
+export * from "./utils/autorouting/SimpleRouteJson"
 
 // Allows easier introspection of render process
 export * from "./components/base-components/Renderable"
