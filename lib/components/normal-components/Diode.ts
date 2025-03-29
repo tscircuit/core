10f2ab6f2d6e44cbb11f142f3fa928cc59d97d4b
Anshgrover23 <ag5989670@gmail.com>
2025-03-29 21:31:21 +0530

    add connection prop to diode

--- lib/components/normal-components/Diode.ts
+++ lib/components/normal-components/Diode.ts
@@ -34,4 +34,44 @@ export class Diode extends NormalComponent<
   anode = this.portMap.pin1
   neg = this.portMap.pin2
   cathode = this.portMap.pin2
+
+  doInitialSchematicComponentRender(): void {
+    const { _parsedProps: props } = this
+
+    super.doInitialSchematicComponentRender()
+  }
+
+  doInitialSourceRender(): void {
+    const { db } = this.root!
+    const { _parsedProps: props } = this
+
+    const source_component = db.source_component.insert({
+      ftype: "simple_diode",
+      name: props.name,
+    })
+
+    this.source_component_id = source_component.source_component_id!
+  }
+
+  doInitialPcbComponentRender() {
+    if (this.root?.pcbDisabled) return
+    const { db } = this.root!
+    const { _parsedProps: props } = this
+
+    const pcb_component = db.pcb_component.insert({
+      center: { x: props.pcbX ?? 0, y: props.pcbY ?? 0 },
+      width: 2, // Default width, adjust as needed
+      height: 3, // Default height, adjust as needed
+      layer: props.layer ?? "top",
+      rotation: props.pcbRotation ?? 0,
+      source_component_id: this.source_component_id!,
+      subcircuit_id: this.getSubcircuit().subcircuit_id ?? undefined,
+    })
+
+    this.pcb_component_id = pcb_component.pcb_component_id
+  }
+
+  doInitialCreateTracesFromProps(): void {
+    this._createTracesFromConnectionsProp()
+  }
 }
