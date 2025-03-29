e68d57a680fbf9bb7a647acb015fd2cccc174fdd
Anshgrover23 <ag5989670@gmail.com>
2025-03-29 21:49:57 +0530

    remove extra pcb render function

--- lib/components/normal-components/Diode.ts
+++ lib/components/normal-components/Diode.ts
@@ -53,24 +53,6 @@ export class Diode extends NormalComponent<
     this.source_component_id = source_component.source_component_id!
   }
 
-  doInitialPcbComponentRender() {
-    if (this.root?.pcbDisabled) return
-    const { db } = this.root!
-    const { _parsedProps: props } = this
-
-    const pcb_component = db.pcb_component.insert({
-      center: { x: props.pcbX ?? 0, y: props.pcbY ?? 0 },
-      width: 2, // Default width, adjust as needed
-      height: 3, // Default height, adjust as needed
-      layer: props.layer ?? "top",
-      rotation: props.pcbRotation ?? 0,
-      source_component_id: this.source_component_id!,
-      subcircuit_id: this.getSubcircuit().subcircuit_id ?? undefined,
-    })
-
-    this.pcb_component_id = pcb_component.pcb_component_id
-  }
-
   doInitialCreateTracesFromProps(): void {
     this._createTracesFromConnectionsProp()
   }
