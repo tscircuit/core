a114324f14edfee6c2fdc6e305d6f31b234599ed
Anshgrover23 <ag5989670@gmail.com>
2025-03-29 22:15:52 +0530

    remove extra methods

--- lib/components/normal-components/Diode.ts
+++ lib/components/normal-components/Diode.ts
@@ -34,26 +34,4 @@ export class Diode extends NormalComponent<
   anode = this.portMap.pin1
   neg = this.portMap.pin2
   cathode = this.portMap.pin2
-
-  doInitialSchematicComponentRender(): void {
-    const { _parsedProps: props } = this
-
-    super.doInitialSchematicComponentRender()
-  }
-
-  doInitialSourceRender(): void {
-    const { db } = this.root!
-    const { _parsedProps: props } = this
-
-    const source_component = db.source_component.insert({
-      ftype: "simple_diode",
-      name: props.name,
-    })
-
-    this.source_component_id = source_component.source_component_id!
-  }
-
-  doInitialCreateTracesFromProps(): void {
-    this._createTracesFromConnectionsProp()
-  }
 }
