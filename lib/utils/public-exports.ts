c9a4206d833abd1b2214df7bac372b43e1e8360b
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-04-15 04:02:28 +0530

    add missing public export for applyEditsToCircuitJson (#737)

--- lib/utils/public-exports.ts
+++ lib/utils/public-exports.ts
@@ -1,3 +1,4 @@
 export * from "./edit-events/apply-edit-events-to-manual-edits-file"
+export * from "./edit-events/apply-edit-events-to-circuit-json"
 export * from "./autorouting/getSimpleRouteJsonFromCircuitJson"
 export * from "./render-events/getPhaseTimingsFromRenderEvents"
