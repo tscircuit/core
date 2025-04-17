f1ca93a1757182c47f2660b956ee88d725a368bb
seveibar <seveibar@gmail.com>
2025-04-16 17:58:20 -0700

    basic test setup

---
+++ tests/internal-selectors/css-select.test.tsx
@@ -0,0 +1,20 @@
+import { selectOne } from "css-select"
+import { test, expect } from "bun:test"
+import { RootCircuit } from "lib"
+
+test("css-select on RootCircuit", () => {
+  const circuit = new RootCircuit()
+  circuit.add(
+    <board width="10mm" height="10mm">
+      <resistor name="R1" resistance="10k" footprint="0402" />
+    </board>,
+  )
+
+  circuit.render()
+
+  const result = selectOne("board > .R1", circuit, {
+    // adapter: ...
+  })
+
+  expect(result).toBeDefined()
+})
