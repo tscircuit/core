e2dde301c62438dcbc0a2f4076b6290fad240e48
seveibar <seveibar@gmail.com>
2025-05-20 15:04:43 -0700

    add test that shows that schematic trace id is not added

---
+++ tests/components/normal-components/schematic-net-label.test.tsx
@@ -0,0 +1,40 @@
+import { expect, test } from "bun:test"
+import { getTestFixture } from "tests/fixtures/get-test-fixture"
+
+test("schematic-net-label", async () => {
+  const { circuit } = await getTestFixture()
+
+  circuit.add(
+    <board>
+      <chip
+        name="U1"
+        footprint="soic4"
+        connections={{
+          pin1: "net.GND",
+        }}
+      />
+    </board>,
+  )
+
+  circuit.render()
+
+  expect(circuit.db.schematic_net_label.list()).toMatchInlineSnapshot(`
+    [
+      {
+        "anchor_position": {
+          "x": -0.6000000000000001,
+          "y": 0.1,
+        },
+        "anchor_side": "right",
+        "center": {
+          "x": -0.6000000000000001,
+          "y": 0.1,
+        },
+        "schematic_net_label_id": "schematic_net_label_0",
+        "source_net_id": "source_net_0",
+        "text": "GND",
+        "type": "schematic_net_label",
+      },
+    ]
+  `)
+})
