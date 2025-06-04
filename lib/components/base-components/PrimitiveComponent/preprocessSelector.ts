d0de75b9bfe678d90dd0d51ad0107b2dce260c23
Severin Ibarluzea <seve700@gmail.com>
2025-06-04 09:08:16 -0700

    Throw error for invalid net selectors

--- lib/components/base-components/PrimitiveComponent/preprocessSelector.ts
+++ lib/components/base-components/PrimitiveComponent/preprocessSelector.ts
@@ -1,4 +1,9 @@
 export const preprocessSelector = (selector: string) => {
+  if (/net\.[^\s>]*\./.test(selector)) {
+    throw new Error(
+      'Net names cannot contain a period, try using "sel.net..." to autocomplete with conventional net names, e.g. V3_3',
+    )
+  }
   return selector
     .replace(/ pin/g, " port")
     .replace(/ subcircuit\./g, " group[isSubcircuit=true]")
