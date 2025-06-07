b6bd9a0ff85cc5f811ce0fbe964d78b68492f321
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-06-07 11:29:54 +0200

    updated dep + fix test

--- tests/components/primitive-components/pcb-rotation.test.tsx
+++ tests/components/primitive-components/pcb-rotation.test.tsx
@@ -2,22 +2,22 @@ import { test, expect } from "bun:test"
 import { getTestFixture } from "tests/fixtures/get-test-fixture"
 
 test("pcb rotation", async () => {
-  const { project, logSoup } = getTestFixture()
+  const { circuit } = getTestFixture()
 
-  project.add(
+  circuit.add(
     <board width={12} height={4}>
-      <resistor name="R1" pcbX={-5} footprint="soic8" resistance="1k" />
+      <resistor name="R1" pcbX={-5} footprint="0402" resistance="1k" />
       <resistor
         name="R2"
         pcbX={5}
-        footprint="soic8"
+        footprint="0402"
         resistance="1k"
         pcbRotation={90}
       />
     </board>,
   )
 
-  project.render()
+  circuit.render()
 
-  expect(project.getCircuitJson()).toMatchPcbSnapshot(import.meta.dir)
+  expect(circuit.getCircuitJson()).toMatchPcbSnapshot(import.meta.path)
 })
