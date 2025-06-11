99bee33979016169fb5883a5613ac628ad457085
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-06-11 23:40:37 +0530

    Include net name in start-with-number error

--- tests/sel/net-name-start-number.test.ts
+++ tests/sel/net-name-start-number.test.ts
@@ -3,6 +3,6 @@ import { test, expect } from "bun:test"
 
 test("preprocessSelector - net name starting with number throws", () => {
   expect(() => preprocessSelector("net.1V")).toThrow(
-    'Net names cannot start with a number, try using a prefix like "VBUS1"',
+    'Net name "1V" cannot start with a number, try using a prefix like "VBUS1"',
   )
 })
