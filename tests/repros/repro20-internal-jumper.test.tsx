2094927d627dc303feee055760c27d16940c65fe
Shibo <shibosoftwaredev@gmail.com>
2025-06-14 14:48:14 +0200

    Add schematic snapshot and enable autolayout in repro

--- tests/repros/repro20-internal-jumper.test.tsx
+++ tests/repros/repro20-internal-jumper.test.tsx
@@ -7,7 +7,7 @@ test("Jumper internallyConnectedPins chooses missing schematic symbol", async ()
   const { circuit } = getTestFixture()
 
   circuit.add(
-    <board width="10mm" height="10mm">
+    <board width="10mm" height="10mm" schAutoLayoutEnabled>
       <solderjumper name="SJ" bridgedPins={[["1", "2"]]} />
       <jumper name="JP1" internallyConnectedPins={[["1", "2"]]} />
       <jumper name="JP2" internallyConnectedPins={[["pin1", "pin2"]]} />
@@ -19,4 +19,6 @@ test("Jumper internallyConnectedPins chooses missing schematic symbol", async ()
     .getCircuitJson()
     .filter((e) => e.type.includes("error"))
   expect(errors.length).toBe(0)
+
+  expect(circuit).toMatchSchematicSnapshot(import.meta.path)
 })
