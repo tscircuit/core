d0c26db9fd50ab947e09737a281bffc1d36a3db1
imrishabh18 <belikerishabh@gmail.com>
2025-06-18 17:59:58 +0530

    remove the board from test

--- tests/groups/group-pcb-padding.test.tsx
+++ tests/groups/group-pcb-padding.test.tsx
@@ -4,24 +4,10 @@ import { getTestFixture } from "tests/fixtures/get-test-fixture"
 test("group pcb padding", () => {
   const { circuit } = getTestFixture()
   circuit.add(
-    <board width="10mm" height="10mm">
-      <group name="G1" subcircuit padding={10}>
-        <resistor
-          name="R1"
-          resistance="1k"
-          footprint="0402"
-          pcbX={-2}
-          pcbY={0}
-        />
-        <resistor
-          name="R2"
-          resistance="1k"
-          footprint="0402"
-          pcbX={2}
-          pcbY={0}
-        />
-      </group>
-    </board>,
+    <group name="G1" subcircuit padding={10}>
+      <resistor name="R1" resistance="1k" footprint="0402" pcbX={-2} pcbY={0} />
+      <resistor name="R2" resistance="1k" footprint="0402" pcbX={2} pcbY={0} />
+    </group>,
   )
 
   circuit.renderUntilSettled()
@@ -30,6 +16,8 @@ test("group pcb padding", () => {
   expect(pcbGroups.length).toBe(1)
   expect(pcbGroups[0].width).toBeCloseTo(25.6, 1)
   expect(pcbGroups[0].height).toBeCloseTo(20.6, 1)
+
+  console.log(circuit.getCircuitJson())
 
   expect(circuit).toMatchPcbSnapshot(import.meta.path)
 })
