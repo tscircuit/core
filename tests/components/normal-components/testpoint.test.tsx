64e19e79568834c1abec4168d49060836e25fef1
techmannih <manishchaudharyttt@gmail.com>
2025-06-28 21:29:41 +0530

    update schematic symbol and add testpoint test with netlabel

--- tests/components/normal-components/testpoint.test.tsx
+++ tests/components/normal-components/testpoint.test.tsx
@@ -22,3 +22,44 @@ test("<testpoint /> component", async () => {
   expect(circuit).toMatchPcbSnapshot(import.meta.path)
   expect(circuit).toMatchSchematicSnapshot(import.meta.path)
 })
+
+test("<testpoint /> component with netlabel test", async () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <board>
+      <testpoint name="TP1" schX={0} schY={0.5} schRotation={180} />
+      <testpoint name="TP2" schX={0} schY={0} schRotation={180} />
+      <testpoint name="TP3" schX={0} schY={-0.5} schRotation={180} />
+      <netlabel
+        schX={0.8}
+        schY={0.9}
+        net="VCC"
+        anchorSide="bottom"
+        connection="TP1.pin1"
+      />
+      <netlabel
+        schX={0.8}
+        schY={0}
+        net="COPI"
+        anchorSide="left"
+        connection="TP2.pin1"
+      />
+      <netlabel
+        schX={0.8}
+        schY={-0.9}
+        net="GND"
+        anchorSide="top"
+        connection="TP3.pin1"
+      />
+    </board>,
+  )
+
+  circuit.render()
+
+  expect(circuit.db.toArray().filter((x) => "error_type" in x)).toEqual([])
+
+  expect(circuit).toMatchSchematicSnapshot(
+    import.meta.path + ".schematic-netlabels",
+  )
+})
