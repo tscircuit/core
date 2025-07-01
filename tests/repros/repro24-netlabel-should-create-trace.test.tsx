e0a67ce0f2c2b8d507d1d73ce760799a2b937516
seveibar <seveibar@gmail.com>
2025-07-01 10:49:49 -0700

    reproduce trace not crossing over net properly

--- tests/repros/repro24-netlabel-should-create-trace.test.tsx
+++ tests/repros/repro24-netlabel-should-create-trace.test.tsx
@@ -20,12 +20,33 @@ test("netlabel autorouting creates pcb traces", async () => {
         schX={-3}
         pcbX={-3}
       />
-      <netlabel net="VCC" connectsTo={["R1.pin1", "C1.pin1"]} />
+      <netlabel
+        net="VCC"
+        connectsTo={["R1.pin1", "R1.pin2", "C1.pin1", "C1.pin2"]}
+      />
     </board>,
   )
 
   await circuit.renderUntilSettled()
-  
+
+  expect(circuit).toMatchPcbSnapshot(import.meta.path)
+
+  // There should be some connection between R1.pin2 and C1.pin1 that goes
+  // through across x=0
+  expect(
+    "\n" +
+      circuit.db.pcb_trace
+        .list()
+        .map((t) => t.route.map((r) => `(${r.x}, ${r.y})`).join("->"))
+        .join("\n"),
+  ).toMatchInlineSnapshot(`
+    "
+    (-2.5, 0)->(-3.5, 0)->(-3.5, 0)
+    (-3.5, 0)->(-2.5, 0)->(-2.5, 0)
+    (3.5, 0)->(2.5, 0)->(2.5, 0)
+    (2.5, 0)->(3.5, 0)->(3.5, 0)"
+  `)
+
   expect(circuit.db.source_trace.list().length).toBeGreaterThan(0)
   expect(circuit.db.pcb_trace.list().length).toBeGreaterThan(0)
 })
