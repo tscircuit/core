4796d107f1634db5aa607a5c5c022bd09c6cba62
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2025-07-18 18:08:21 +0200

    Added repro for externally connected pins bug (#1079)
    * Added repro for externally connected pins bug
    
    * Added repro for externally connected pins bug

---
+++ tests/repros/repro29-externally-connected-pins.test.tsx
@@ -0,0 +1,51 @@
+import { test, expect } from "bun:test"
+import { getTestFixture } from "../fixtures/get-test-fixture"
+
+test.skip("chip with externally connected pins repro", async () => {
+  const { circuit } = getTestFixture()
+  const pinLabels = {
+    pin1: ["VDDIO", "V3_3"],
+    pin2: ["NC1"],
+    pin3: ["RESERVED1"],
+    pin4: ["SCLK"],
+    pin5: ["RESERVED2"],
+    pin6: ["MOSI"],
+    pin7: ["MISO"],
+    pin8: ["N_CS"],
+    pin9: ["INT2"],
+    pin10: ["RESERVED3"],
+    pin11: ["INT1"],
+    pin12: ["GND1"],
+    pin13: ["GND2"],
+    pin14: ["VS"],
+    pin15: ["NC2"],
+    pin16: ["GND3"],
+  } as const
+  circuit.add(
+    <board width="20mm" height="15mm" routingDisabled={true}>
+      <chip
+        name="U1"
+        schWidth={1.7}
+        footprint="pinrow16"
+        externallyConnectedPins={[["pin12", "pin13"]]}
+        pinLabels={pinLabels}
+        schPinArrangement={{
+          rightSide: {
+            direction: "top-to-bottom",
+            pins: [1, 14, 11, 9],
+          },
+          leftSide: {
+            direction: "top-to-bottom",
+            pins: [4, 6, 7, 8],
+          },
+          bottomSide: {
+            direction: "left-to-right",
+            pins: [12],
+          },
+        }}
+      />
+    </board>,
+  )
+
+  circuit.render()
+})
