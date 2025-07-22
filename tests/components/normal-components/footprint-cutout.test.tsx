b39936daa3fdf27e6bd566eb527282ccdf735fbb
Abimael Martell <abimex@gmail.com>
2025-07-22 16:47:45 -0700

    test snapshot

---
+++ tests/components/normal-components/footprint-cutout.test.tsx
@@ -0,0 +1,16 @@
+import { test, expect } from "bun:test"
+import { getTestFixture } from "tests/fixtures/get-test-fixture"
+
+test("footprint cutout", async () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <board>
+      <chip name="m2host" footprint="m2host" />
+    </board>,
+  )
+
+  await circuit.renderUntilSettled()
+
+  expect(circuit).toMatchPcbSnapshot(import.meta.path)
+})
