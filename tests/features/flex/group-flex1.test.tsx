525c05463404c77689a253dcc5aa3e596e31299d
imrishabh18 <belikerishabh@gmail.com>
2025-07-23 00:24:51 -0700

    Add support for flex

---
+++ tests/features/flex/group-flex1.test.tsx
@@ -0,0 +1,17 @@
+import { test, expect } from "bun:test"
+import { getTestFixture } from "tests/fixtures/get-test-fixture"
+
+test("group-flex1", () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <group subcircuit flex width="10mm" height="10mm" routingDisabled>
+      <resistor name="R1" resistance="1k" footprint="0402" />
+      <resistor name="R2" resistance="1k" footprint="0402" />
+    </group>,
+  )
+
+  circuit.render()
+
+  expect(circuit).toMatchPcbSnapshot(import.meta.path)
+})
