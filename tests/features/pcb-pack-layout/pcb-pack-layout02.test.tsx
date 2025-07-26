06208edc144351dae61094736ecf3e73490a1867
Severin Ibarluzea <seve700@gmail.com>
2025-07-25 18:37:40 -0700

    WIP Nested Packing Support, update calculate-packing to support packing without connections (#1100)
    * nested group pack test (not working)
    
    * fix packing failing without connections

---
+++ tests/features/pcb-pack-layout/pcb-pack-layout02.test.tsx
@@ -0,0 +1,18 @@
+import { test, expect } from "bun:test"
+import { getTestFixture } from "tests/fixtures/get-test-fixture"
+import { writeGlobalDebugGraphics } from "tests/fixtures/writeGlobalDebugGraphics"
+
+test("pcb-pack-layout01", async () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <board pack gap="1.5mm">
+      <chip footprint="soic8" name="U1" />
+      <resistor footprint="0402" name="R1" resistance="1k" />
+      <capacitor footprint="0603" name="C1" capacitance="100nF" />
+    </board>,
+  )
+
+  expect(circuit).toMatchPcbSnapshot(import.meta.path)
+  writeGlobalDebugGraphics()
+})
