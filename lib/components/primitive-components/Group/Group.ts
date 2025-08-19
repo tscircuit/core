3f9329aa594b5d63142f28570287039536fe56ac
Abimael Martell <abimex@gmail.com>
2025-08-18 17:11:58 -0700

    format

--- lib/components/primitive-components/Group/Group.ts
+++ lib/components/primitive-components/Group/Group.ts
@@ -154,15 +154,20 @@ export class Group<Props extends z.ZodType<any, any, any> = typeof groupProps>
       // Check if the group has explicit pcbX/pcbY positioning
       // If so, preserve the center position instead of overriding with bounds calculation
       const currentPcbGroup = db.pcb_group.get(this.pcb_group_id)
-      const hasExplicitPositioning = this._parsedProps.pcbX !== undefined || this._parsedProps.pcbY !== undefined
-      
+      const hasExplicitPositioning =
+        this._parsedProps.pcbX !== undefined ||
+        this._parsedProps.pcbY !== undefined
+
       db.pcb_group.update(this.pcb_group_id, {
         width: Number(props.width ?? width),
         height: Number(props.height ?? height),
-        center: hasExplicitPositioning && currentPcbGroup ? currentPcbGroup.center : {
-          x: centerX,
-          y: centerY,
-        },
+        center:
+          hasExplicitPositioning && currentPcbGroup
+            ? currentPcbGroup.center
+            : {
+                x: centerX,
+                y: centerY,
+              },
       })
     }
   }
