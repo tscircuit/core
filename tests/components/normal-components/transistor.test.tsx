4712ee00f97baddb7e7fc190441c8b10675a33e0
seveibar <seveibar@gmail.com>
2025-08-27 15:18:11 -0700

    transistor migration to msp routing

--- tests/components/normal-components/transistor.test.tsx
+++ tests/components/normal-components/transistor.test.tsx
@@ -4,6 +4,7 @@ import { Transistor } from "lib/components/normal-components/Transistor"
 
 it("should render a PNP and NPN transistor", async () => {
   const { circuit } = getTestFixture()
+  circuit._featureMspSchematicTraceRouting = true
   circuit.add(
     <board width="10mm" height="10mm">
       <transistor
