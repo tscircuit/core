35d1b009be6c702d9c2f44da3534c41c4110e113
seveibar <seveibar@gmail.com>
2025-08-27 14:12:48 -0700

    work in progress, not skipping ports

--- lib/components/primitive-components/Group/Group_doInitialSchematicTraceRender/getNetNameFromPorts.ts
+++ lib/components/primitive-components/Group/Group_doInitialSchematicTraceRender/getNetNameFromPorts.ts
@@ -5,6 +5,18 @@ import { Port } from "../../Port"
  * trace can't be routed.
  */
 export const getNetNameFromPorts = (ports: Port[]): string => {
+  // Are any of these ports connected to a trace with a display label?
+  for (const port of ports) {
+    const traces = port._getDirectlyConnectedTraces()
+
+    for (const trace of traces) {
+      const displayLabel = trace._parsedProps.schDisplayLabel
+      if (displayLabel) {
+        return displayLabel
+      }
+    }
+  }
+
   const netName = ports.map((p) => p._getNetLabelText()).join("/")
   return netName
 }
