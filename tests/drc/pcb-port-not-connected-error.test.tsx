b68d2d53bda0165da2f96d1fa39cbbad2c66797a
Shibo <shibosoftwaredev@gmail.com>
2025-08-29 16:58:53 +0200

    implement square wave voltagesource (#1212)
    * implement square wave voltagesource
    
    * update ss
    
    * merge from main
    
    * updated test

--- tests/drc/pcb-port-not-connected-error.test.tsx
+++ tests/drc/pcb-port-not-connected-error.test.tsx
@@ -15,13 +15,21 @@ test("check each PCB port is connected", async () => {
       }}
     >
       <resistor name="R1" resistance="10k" footprint="0402" pcbX={0} pcbY={0} />
-      <resistor name="R2" resistance="10k" footprint="0402" pcbX={5} pcbY={0} />
+      <resistor
+        name="R2"
+        resistance="10k"
+        footprint="0402"
+        schX={3}
+        pcbX={5}
+        pcbY={0}
+      />
       <resistor
         name="R3"
         resistance="10k"
         footprint="0402"
         pcbX={10}
         pcbY={0}
+        schX={-3}
       />
       <trace from=".R1 > .pin1" to=".R2 > .pin1" />
       <trace from=".R2 > .pin2" to=".R3 > .pin1" />
