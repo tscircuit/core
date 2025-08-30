23e060dbe4d62a9757fc81cfbb8637c1ebf3ad8a
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2025-08-30 22:44:41 +0200

    Ensure shared and global net labels render in subcircuits

--- tests/components/normal-components/chip-with-subcircuit-net-label.test.tsx
+++ tests/components/normal-components/chip-with-subcircuit-net-label.test.tsx
@@ -7,9 +7,6 @@ it("subcircuit having a net label GND makes the circuit fail to use GND in other
 
   circuit.add(
     <board width="10mm" height="10mm">
-      <group subcircuit>
-        <net name="GND" />
-      </group>
       <chip
         name="U1"
         footprint="soic8"
@@ -28,5 +25,59 @@ it("subcircuit having a net label GND makes the circuit fail to use GND in other
 
   circuit.render()
 
-  expect(circuit.getCircuitJson()).toMatchSchematicSnapshot(import.meta.dir)
+  expect(circuit.getCircuitJson()).toMatchSchematicSnapshot(
+    import.meta.dir + "-one-chip",
+  )
+})
+
+it("should keep local nets isolated between adjacent subcircuits", async () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <board
+      width="30mm"
+      height="30mm"
+      schLayout={{
+        flex: true,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
+      <group name="SubA" subcircuit>
+        <chip
+          name="U_A"
+          footprint="soic8"
+          pinLabels={{ "1": "IN_A", "2": "OUT_A", "3": "INTERNAL", "8": "GND" }}
+          schPinArrangement={{ leftSize: 4, rightSize: 4 }}
+        />
+        <net name="INTERNAL" />
+        <trace from=".U_A > .3" to="net.INTERNAL" />
+      </group>
+      <group name="SubB" subcircuit>
+        <chip
+          name="U_B"
+          footprint="soic8"
+          schX={8}
+          pinLabels={{ "1": "IN_B", "2": "OUT_B", "3": "INTERNAL", "8": "GND" }}
+          schPinArrangement={{ leftSize: 4, rightSize: 4 }}
+        />
+        <net name="INTERNAL" />
+        <trace from=".U_B > .3" to="net.INTERNAL" />
+      </group>
+
+      <net name="EXTERNAL" />
+      <trace from=".SubA > .U_A > .2" to="net.EXTERNAL" />
+      <trace from=".SubB > .U_B > .2" to="net.EXTERNAL" />
+
+      <trace from=".SubA > .U_A > .8" to="net.GND" />
+      <trace from=".SubB > .U_B > .8" to="net.GND" />
+    </board>,
+  )
+
+  circuit.render()
+
+  expect(circuit.getCircuitJson()).toMatchSchematicSnapshot(
+    import.meta.dir + "-two-chips",
+  )
 })
