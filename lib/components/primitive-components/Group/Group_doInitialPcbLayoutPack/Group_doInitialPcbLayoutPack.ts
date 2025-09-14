a154cc6eb6cf91b7686831b685099d4cc801e897
seveibar <seveibar@gmail.com>
2025-09-14 11:59:13 -0700

    rewrite pack algo

--- lib/components/primitive-components/Group/Group_doInitialPcbLayoutPack/Group_doInitialPcbLayoutPack.ts
+++ lib/components/primitive-components/Group/Group_doInitialPcbLayoutPack/Group_doInitialPcbLayoutPack.ts
@@ -27,54 +27,49 @@ export const Group_doInitialPcbLayoutPack = (group: Group) => {
     pcbPackGap,
   } = props
 
-  const gap = pcbPackGap ?? pcbGap ?? gapProp
-  const gapMm = length.parse(gap ?? DEFAULT_MIN_GAP)
+  // const gap = pcbPackGap ?? pcbGap ?? gapProp
+  // const gapMm = length.parse(gap ?? DEFAULT_MIN_GAP)
 
-  const chipMarginsMap: Record<
-    string,
-    { left: number; right: number; top: number; bottom: number }
-  > = {}
+  // const chipMarginsMap: Record<
+  //   string,
+  //   { left: number; right: number; top: number; bottom: number }
+  // > = {}
 
-  const collectMargins = (comp: any) => {
-    if (comp?.pcb_component_id && comp?._parsedProps) {
-      const props = comp._parsedProps
-      const left = length.parse(props.pcbMarginLeft ?? props.pcbMarginX ?? 0)
-      const right = length.parse(props.pcbMarginRight ?? props.pcbMarginX ?? 0)
-      const top = length.parse(props.pcbMarginTop ?? props.pcbMarginY ?? 0)
-      const bottom = length.parse(
-        props.pcbMarginBottom ?? props.pcbMarginY ?? 0,
-      )
-      if (left || right || top || bottom) {
-        chipMarginsMap[comp.pcb_component_id] = { left, right, top, bottom }
-      }
-    }
-    if (comp?.children) comp.children.forEach(collectMargins)
-  }
+  // const collectMargins = (comp: any) => {
+  //   if (comp?.pcb_component_id && comp?._parsedProps) {
+  //     const props = comp._parsedProps
+  //     const left = length.parse(props.pcbMarginLeft ?? props.pcbMarginX ?? 0)
+  //     const right = length.parse(props.pcbMarginRight ?? props.pcbMarginX ?? 0)
+  //     const top = length.parse(props.pcbMarginTop ?? props.pcbMarginY ?? 0)
+  //     const bottom = length.parse(
+  //       props.pcbMarginBottom ?? props.pcbMarginY ?? 0,
+  //     )
+  //     if (left || right || top || bottom) {
+  //       chipMarginsMap[comp.pcb_component_id] = { left, right, top, bottom }
+  //     }
+  //   }
+  //   if (comp?.children) comp.children.forEach(collectMargins)
+  // }
 
-  collectMargins(group)
-  const packInput: PackInput = {
-    ...convertPackOutputToPackInput(
-      convertCircuitJsonToPackOutput(db.toArray(), {
-        source_group_id: group.source_group_id!,
-        shouldAddInnerObstacles: true,
-        chipMarginsMap,
-      }),
-    ),
-    // @ts-expect-error we're missing some pack order strategies
-    orderStrategy: packOrderStrategy ?? "largest_to_smallest",
-    placementStrategy:
-      packPlacementStrategy ?? "minimum_sum_squared_distance_to_network",
-    minGap: gapMm,
-  }
+  // collectMargins(group)
+  // const packInput: PackInput = {
+  //   ...convertPackOutputToPackInput(
+  //     convertCircuitJsonToPackOutput(db.toArray(), {
+  //       source_group_id: group.source_group_id!,
+  //       shouldAddInnerObstacles: true,
+  //       chipMarginsMap,
+  //     }),
+  //   ),
+  //   // @ts-expect-error we're missing some pack order strategies
+  //   orderStrategy: packOrderStrategy ?? "largest_to_smallest",
+  //   placementStrategy:
+  //     packPlacementStrategy ?? "minimum_sum_squared_distance_to_network",
+  //   minGap: gapMm,
+  // }
 
-  const clusterMap = applyComponentConstraintClusters(group, packInput)
+  // const clusterMap = applyComponentConstraintClusters(group, packInput)
 
   if (debug.enabled) {
-    group.root?.emit("debug:logOutput", {
-      type: "debug:logOutput",
-      name: `packInput-circuitjson-${group.name}`,
-      content: JSON.stringify(db.toArray()),
-    })
     group.root?.emit("debug:logOutput", {
       type: "debug:logOutput",
       name: `packInput-${group.name}`,
