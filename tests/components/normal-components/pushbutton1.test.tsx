d41fad7d531da1c77bf14f36b5b5f23bf5b04780
Samarth Shukla <paarthsamarth@gmail.com>
2025-09-15 13:48:52 +0530

    check

--- tests/components/normal-components/pushbutton1.test.tsx
+++ tests/components/normal-components/pushbutton1.test.tsx
@@ -1,17 +1,25 @@
 import { test, expect } from "bun:test"
 import { getTestFixture } from "tests/fixtures/get-test-fixture"
+import { Board } from "lib/components/board"
+import { Pushbutton } from "lib/components/pushbutton"
 
 test("<pushbutton /> component", async () => {
   const { circuit } = getTestFixture()
 
   circuit.add(
-    <board width="12mm" height="10mm">
-      <pushbutton name="PB1" footprint="pushbutton" pcbX={0} pcbY={0} />
-    </board>,
+    <Board width={12} height={10}>
+      <Pushbutton 
+        name="PB1" 
+        footprint="PUSHBUTTON_THT" 
+        pcbX={0} 
+        pcbY={0}
+        pin1="GND"
+        pin2="VCC"
+      />
+    </Board>
   )
 
   await circuit.renderUntilSettled()
-
   expect(circuit).toMatchPcbSnapshot(import.meta.path)
   expect(circuit).toMatchSchematicSnapshot(import.meta.path)
 })
