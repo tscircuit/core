9becde99ddee23c5b7d01d3ad802274e68c010d2
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-09-23 15:34:13 +0530

    support rotation of group (#1399)

---
+++ tests/repros/repro56-rotate-group.test.tsx
@@ -0,0 +1,31 @@
+import { expect, test } from "bun:test"
+import { getTestFixture } from "tests/fixtures/get-test-fixture"
+
+test("rotate group and have traces", async () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <board width="60mm" height="20mm">
+      <group name="G0" pcbRotation="0deg" pcbX={-18}>
+        <resistor name="R1" resistance="1k" footprint="0402" pcbX={-1} />
+        <resistor name="R2" resistance="1k" footprint="0402" pcbX={1} />
+      </group>
+
+      <group name="G1" pcbRotation="45deg">
+        <resistor name="R3" resistance="1k" footprint="0402" pcbX={-1} />
+        <resistor name="R4" resistance="1k" footprint="0402" pcbX={1} />
+      </group>
+
+      <trace from="G0.R1 > .pin1" to="G1.R3 > .pin1" />
+    </board>,
+  )
+
+  await circuit.renderUntilSettled()
+
+  const circuitJson = circuit.getCircuitJson()
+
+  const pcb_trace = circuitJson.filter((c) => c.type === "pcb_trace")
+  expect(pcb_trace.length).toBe(1)
+
+  expect(circuitJson).toMatchPcbSnapshot(import.meta.path)
+})
