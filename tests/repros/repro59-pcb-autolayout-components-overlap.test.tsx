13c81ea9a3d237f38bcde0bee5758280ecb43c45
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2025-09-28 09:22:07 +0200

    Add test for PCB autolayout components overlap scenario (#1415)

---
+++ tests/repros/repro59-pcb-autolayout-components-overlap.test.tsx
@@ -0,0 +1,44 @@
+import { expect, test } from "bun:test"
+import { getTestFixture } from "tests/fixtures/get-test-fixture"
+import { sel } from "tscircuit"
+
+test("pcb autolayout components overlap", async () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <board width="10mm" height="10mm">
+      <group>
+        <resistor
+          name="R1"
+          resistance="2.2k"
+          footprint="0603"
+          schRotation={90}
+          connections={{ pin1: sel.U1.pin9, pin2: sel.JP1.pin3 }}
+        />
+        <resistor
+          name="R2"
+          resistance="2.2k"
+          footprint="0603"
+          schRotation={90}
+          connections={{ pin1: sel.U1.pin10, pin2: sel.JP1.pin1 }}
+        />
+      </group>
+      <jumper
+        pcbX={0}
+        pcbY={0}
+        name="JP3"
+        footprint="pinrow4_id1.016_od1.88_nosquareplating_pinlabeltextalignright_pinlabelorthogonal_doublesidedpinlabel_pinlabelverticallyinverted"
+        connections={{ pin3: sel.U1.pin10 }}
+      />
+    </board>,
+  )
+
+  await circuit.renderUntilSettled()
+
+  const circuitJson = circuit.getCircuitJson()
+
+  // TODO uncomment when fixed
+  //expect(circuit.db.pcb_component_outside_board_error.list()).toEqual([])
+
+  expect(circuitJson).toMatchPcbSnapshot(import.meta.path)
+})
