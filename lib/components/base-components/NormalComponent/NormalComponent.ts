eb42881cbb4b4818f4079faf6ebf158ccc5ca110
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-10-12 19:34:57 +0200

    patch

--- lib/components/base-components/NormalComponent/NormalComponent.ts
+++ lib/components/base-components/NormalComponent/NormalComponent.ts
@@ -1249,12 +1249,16 @@ export class NormalComponent<
     const cadModelProp = this._parsedProps.cadModel
     const cadModel =
       cadModelProp === undefined ? this._asyncFootprintCadModel : cadModelProp
-    const footprint =
-      this.props.footprint && this.props.color
-        ? `${this.props.footprint}_${this.props.color}`
-        : this.props.footprint
-          ? this.props.footprint
-          : this._getImpliedFootprintString()
+    let footprint: string | null | undefined
+    if (
+      this.constructor.name === "Led" &&
+      this.props.footprint &&
+      this.props.color
+    ) {
+      footprint = `${this.props.footprint}_${this.props.color}`
+    } else {
+      footprint = this.props.footprint ?? this._getImpliedFootprintString()
+    }
 
     if (!this.pcb_component_id) return
     if (!cadModel && !footprint) return
