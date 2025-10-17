0697137c081be4faf0fa27952b5fe3a779dd7810
imrishabh18 <belikerishabh@gmail.com>
2025-10-17 20:03:23 +0530

    remove full snapshot check

--- tests/features/pcb-pack-layout/pack-inside-bounds.test.tsx
+++ tests/features/pcb-pack-layout/pack-inside-bounds.test.tsx
@@ -15,45 +15,5 @@ test("pack inside group width and height specified", async () => {
 
   await circuit.renderUntilSettled()
 
-  const pcb_components = circuit.db.pcb_component.list()
-  expect(pcb_components).toMatchInlineSnapshot(`
-    [
-      {
-        "center": {
-          "x": 2.5,
-          "y": -2.5,
-        },
-        "do_not_place": false,
-        "height": 0.6399999999999997,
-        "layer": "top",
-        "obstructs_within_bounds": true,
-        "pcb_component_id": "pcb_component_0",
-        "pcb_group_id": "pcb_group_0",
-        "rotation": 0,
-        "source_component_id": "source_component_0",
-        "subcircuit_id": "subcircuit_source_group_0",
-        "type": "pcb_component",
-        "width": 1.5599999999999998,
-      },
-      {
-        "center": {
-          "x": 2.5,
-          "y": -0.8600000000000001,
-        },
-        "do_not_place": false,
-        "height": 0.6399999999999997,
-        "layer": "top",
-        "obstructs_within_bounds": true,
-        "pcb_component_id": "pcb_component_1",
-        "pcb_group_id": "pcb_group_0",
-        "rotation": 0,
-        "source_component_id": "source_component_1",
-        "subcircuit_id": "subcircuit_source_group_0",
-        "type": "pcb_component",
-        "width": 1.5599999999999998,
-      },
-    ]
-  `)
-
   expect(circuit).toMatchPcbSnapshot(import.meta.path)
 })
