7e71a949460f65471579dec7b410ca783ba4b341
Severin Ibarluzea <seve700@gmail.com>
2025-10-30 22:43:14 -0700

    Add PCB snapshot to assignable via autorouting test

--- tests/utils/autorouting/simple-route-json-assignable-via.test.tsx
+++ tests/utils/autorouting/simple-route-json-assignable-via.test.tsx
@@ -26,6 +26,8 @@ test("assignable vias produce assignable obstacles", async () => {
 
   await circuit.renderUntilSettled()
 
+  await expect(circuit).toMatchPcbSnapshot(import.meta.path)
+
   const { simpleRouteJson } = getSimpleRouteJsonFromCircuitJson({
     db: circuit.db,
   })
