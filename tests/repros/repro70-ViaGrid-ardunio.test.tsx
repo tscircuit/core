36068ff3dd09c72b393196784d40f7b4eb71bf6b
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-11-05 02:28:38 +0530

    add repro test for ViaGridBoard with autoplacement (#1619)
    * add repro test for ViaGridBoard with autoplacement
    
    * remove log

---
+++ tests/repros/repro70-ViaGrid-ardunio.test.tsx
@@ -0,0 +1,21 @@
+import { expect, test } from "bun:test"
+import { getTestFixture } from "tests/fixtures/get-test-fixture"
+import { ViaGridBoard } from "@tscircuit/common"
+import { RP2040 } from "tests/fixtures/assets/RP2040.tsx"
+
+test("ViaGridBoard ", async () => {
+  const { circuit } = getTestFixture()
+
+  circuit.add(
+    <ViaGridBoard name="VIA_GRID_BOARD">
+      <resistor name="R1" resistance="10k" footprint="0402" />
+      <RP2040 name="U1" />
+    </ViaGridBoard>,
+  )
+
+  await circuit.renderUntilSettled()
+
+  const circuitJson = circuit.getCircuitJson()
+
+  expect(circuit).toMatchPcbSnapshot(import.meta.path)
+})
