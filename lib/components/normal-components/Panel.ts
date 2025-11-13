3665e7f70d1e9ebfc57423d90da90dae4d6f22a0
Ayush Jhawar <111112495+Ayushjhawar8@users.noreply.github.com>
2025-11-13 11:03:06 +0530

    Add pcb_panel center handling (#1650)

--- lib/components/normal-components/Panel.ts
+++ lib/components/normal-components/Panel.ts
@@ -1,8 +1,11 @@
 import { panelProps } from "@tscircuit/props"
+import { distance } from "circuit-json"
 import type { PrimitiveComponent } from "../base-components/PrimitiveComponent"
 import { Group } from "../primitive-components/Group/Group"
 
 export class Panel extends Group<typeof panelProps> {
+  pcb_panel_id: string | null = null
+
   get config() {
     return {
       componentName: "Panel",
@@ -11,6 +14,10 @@ export class Panel extends Group<typeof panelProps> {
   }
 
   get isGroup() {
+    return true
+  }
+
+  get isSubcircuit() {
     return true
   }
 
@@ -27,5 +34,44 @@ export class Panel extends Group<typeof panelProps> {
     }
 
     super.runRenderCycle()
+  }
+
+  doInitialPcbComponentRender() {
+    super.doInitialPcbComponentRender()
+    if (this.root?.pcbDisabled) return
+
+    const { db } = this.root!
+    const props = this._parsedProps
+
+    const inserted = db.pcb_panel.insert({
+      width: distance.parse(props.width),
+      height: distance.parse(props.height),
+      center: this._getGlobalPcbPositionBeforeLayout(),
+      covered_with_solder_mask: !(props.noSolderMask ?? false),
+    })
+
+    this.pcb_panel_id = inserted.pcb_panel_id
+  }
+
+  updatePcbComponentRender() {
+    if (this.root?.pcbDisabled) return
+    if (!this.pcb_panel_id) return
+
+    const { db } = this.root!
+    const props = this._parsedProps
+
+    db.pcb_panel.update(this.pcb_panel_id, {
+      width: distance.parse(props.width),
+      height: distance.parse(props.height),
+      center: this._getGlobalPcbPositionBeforeLayout(),
+      covered_with_solder_mask: !(props.noSolderMask ?? false),
+    })
+  }
+
+  removePcbComponentRender() {
+    if (!this.pcb_panel_id) return
+
+    this.root?.db.pcb_panel.delete(this.pcb_panel_id)
+    this.pcb_panel_id = null
   }
 }
