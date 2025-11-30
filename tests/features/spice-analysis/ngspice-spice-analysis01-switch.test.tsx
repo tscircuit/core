f82cf35a194a63d336b2e62c55b44ecde033012a
Shibo <shibosoftwaredev@gmail.com>
2025-11-30 23:19:53 +0200

    spice support for npn transistors (#1700)

--- tests/features/spice-analysis/ngspice-spice-analysis01-switch.test.tsx
+++ tests/features/spice-analysis/ngspice-spice-analysis01-switch.test.tsx
@@ -14,18 +14,32 @@ test(
     circuit.add(
       <board schMaxTraceDistance={10} routingDisabled>
         <voltagesource name="V1" voltage="5V" />
-        <switch name="SW1" spst simSwitchFrequency="1kHz" />
-        <trace from=".V1 > .terminal1" to=".SW1 > .pin1" />
-        <resistor
-          name="R1"
-          resistance="1k"
-          footprint="0402"
-          connections={{ pin1: ".SW1 > .pin2", pin2: ".V1 > .terminal2" }}
+        <resistor name="R_base" resistance="10k" schY={2} />
+        <switch name="SW1" simSwitchFrequency="1kHz" schX={1.5} schY={2} />
+        <transistor
+          name="Q1"
+          type="npn"
+          footprint="sot23"
+          schX={2}
+          schY={0.3}
+          schRotation={180}
         />
-        <voltageprobe connectsTo={".R1 > .pin1"} />
+        <resistor name="R_collector" resistance="10k" schY={-2} />
+
+        <trace from=".V1 > .pin1" to=".R_base > .pin1" />
+        <trace from=".R_base > .pin2" to=".SW1 > .pin1" />
+        <trace from=".SW1 > .pin2" to=".Q1 > .base" />
+
+        <trace from=".V1 > .pin1" to=".R_collector > .pin1" />
+        <trace from=".R_collector > .pin2" to=".Q1 > .collector" />
+
+        <trace from=".Q1 > .emitter" to=".V1 > .pin2" />
+
+        <voltageprobe name="VP_COLLECTOR" connectsTo=".R_collector > .pin2" />
+
         <analogsimulation
           duration="4ms"
-          timePerStep="10us"
+          timePerStep="50us"
           spiceEngine="ngspice"
         />
       </board>,
