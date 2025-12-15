75d60bd5e47afbedae6a7f8542272809e8c9973f
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-12-15 20:57:51 +0200

    patch

--- lib/components/primitive-components/Group/Group.ts
+++ lib/components/primitive-components/Group/Group.ts
@@ -132,7 +132,6 @@ export class Group<Props extends z.ZodType<any, any, any> = typeof groupProps>
       name: this.name,
       anchor_position: this._getGlobalPcbPositionBeforeLayout(),
       center: this._getGlobalPcbPositionBeforeLayout(),
-      anchor_alignment: "center",
       ...(hasOutline ? { outline: numericOutline } : { width: 0, height: 0 }),
       pcb_component_ids: [],
       source_group_id: this.source_group_id!,
