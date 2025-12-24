48e511f8558528ad1b9cae12877a31b89fd1d58c
seveibar <seveibar@gmail.com>
2025-12-23 17:27:26 -0800

    skip example 29

--- tests/examples/example29-pcb_component_outside_board_error.test.tsx
+++ tests/examples/example29-pcb_component_outside_board_error.test.tsx
@@ -1,7 +1,7 @@
 import { test, expect } from "bun:test"
 import { getTestFixture } from "tests/fixtures/get-test-fixture"
 
-test("example29: component outside board", async () => {
+test.skip("example29: component outside board", async () => {
   const { circuit } = getTestFixture()
 
   circuit.add(
