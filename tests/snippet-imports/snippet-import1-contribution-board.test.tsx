1031849618c6ff92370ff8db53f04370ee83f26c
seveibar <seveibar@gmail.com>
2025-12-23 17:30:38 -0800

    skip test

--- tests/snippet-imports/snippet-import1-contribution-board.test.tsx
+++ tests/snippet-imports/snippet-import1-contribution-board.test.tsx
@@ -4,15 +4,12 @@ import { Circuit } from "lib"
 import * as tscircuitCore from "lib"
 
 test.skip("snippet-import1-contribution-board", async () => {
-  const ContributionBoard = await importSnippet("seveibar/contribution-board", {
-    dependencies: {
-      "@tscircuit/core": tscircuitCore,
-    },
-  })
-
-  const circuit = new Circuit()
-
-  circuit.add(<ContributionBoard />)
-
-  const circuitJson = circuit.getCircuitJson()
+  // const ContributionBoard = await importSnippet("seveibar/contribution-board", {
+  //   dependencies: {
+  //     "@tscircuit/core": tscircuitCore,
+  //   },
+  // })
+  // const circuit = new Circuit()
+  // circuit.add(<ContributionBoard />)
+  // const circuitJson = circuit.getCircuitJson()
 })
