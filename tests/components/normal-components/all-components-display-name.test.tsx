8028daa677a55410bc309da3b55debd8b9b26f3f
techmannih <manishchaudharyttt@gmail.com>
2026-01-22 16:31:27 +0530

    update

--- tests/components/normal-components/all-components-display-name.test.tsx
+++ tests/components/normal-components/all-components-display-name.test.tsx
@@ -1,136 +1,120 @@
 import { test, expect } from "bun:test"
 import { getTestFixture } from "../../fixtures/get-test-fixture"
 
-test("all normal components source render with displayName", async () => {
+test("all normal components source render with DisplayName", async () => {
   const { circuit } = getTestFixture()
 
   circuit.add(
-    <board width={40} height={40}>
-      {/* Row 1 */}
-      <battery name="B1" displayName="My Battery" schX={0} schY={0} />
+    <board width={50} height={30}>
+      {/* Row 1 - Y=0 */}
+      <battery name="B1" displayName="BatteryDisplayName" schX={0} schY={0} />
       <capacitor
         name="C1"
-        displayName="My Capacitor"
+        displayName="CapacitorDisplayName"
         capacitance="10uF"
-        schX={5}
+        schX={6}
         schY={0}
       />
-      <chip name="U1" displayName="My Chip" schX={10} schY={0} />
       <crystal
         name="Y1"
-        displayName="My Crystal"
+        displayName="CrystalDisplayName"
         frequency="10MHz"
         loadCapacitance="10pF"
-        schX={15}
+        schX={12}
         schY={0}
       />
       <currentsource
         name="I1"
-        displayName="My CurrentSource"
+        displayName="CurrentSourceDisplayName"
         current="1A"
-        schX={20}
+        schX={18}
         schY={0}
       />
+      <diode name="D1" displayName="Diode" schX={24} schY={0} />
 
-      {/* Row 2 */}
-      <diode name="D1" displayName="My Diode" schX={0} schY={5} />
+      {/* Row 2 - Y=6 */}
       <fuse
         name="F1"
-        displayName="My Fuse"
+        displayName="FuseDisplayName"
         currentRating="1A"
         voltageRating="10V"
-        schX={5}
-        schY={5}
+        schX={0}
+        schY={6}
       />
       <inductor
         name="L1"
-        displayName="My Inductor"
+        displayName="InductorDisplayName"
         inductance="10uH"
-        schX={10}
-        schY={5}
+        schX={6}
+        schY={6}
       />
-      <interconnect
-        name="J1"
-        displayName="My Interconnect"
-        schX={15}
-        schY={5}
-      />
-      <jumper name="JP1" displayName="My Jumper" schX={20} schY={5} />
-
-      {/* Row 3 */}
-      <led name="LED1" displayName="My Led" schX={0} schY={10} />
+      <led name="LED1" displayName="LedDisplayName" schX={12} schY={6} />
       <mosfet
         name="Q1"
-        displayName="My Mosfet"
+        displayName="MosfetDisplayName"
         channelType="n"
         mosfetMode="enhancement"
-        schX={5}
-        schY={10}
+        schX={18}
+        schY={6}
       />
-      <opamp name="U2" displayName="My OpAmp" schX={10} schY={10} />
-      <pinheader
-        name="J2"
-        displayName="My PinHeader"
-        pinCount={2}
-        schX={15}
-        schY={10}
-      />
-      <pinout name="U3" displayName="My Pinout" schX={20} schY={10} />
-
-      {/* Row 4 */}
       <potentiometer
         name="R1"
-        displayName="My Potentiometer"
+        displayName="PotentiometerDisplayName"
         maxResistance="10k"
+        schX={24}
+        schY={6}
+      />
+
+      {/* Row 3 - Y=12 */}
+      <pushbutton
+        name="SW1"
+        displayName="PushButtonDisplayName"
         schX={0}
-        schY={15}
+        schY={12}
       />
-      <powersource
-        name="V1"
-        displayName="My PowerSource"
-        voltage="5V"
-        schX={5}
-        schY={15}
-      />
-      <pushbutton name="SW1" displayName="My PushButton" schX={10} schY={15} />
       <resistor
         name="R2"
-        displayName="My Resistor"
+        displayName="ResistorDisplayName"
         resistance="1k"
-        schX={15}
-        schY={15}
+        schX={6}
+        schY={12}
       />
       <resonator
         name="Y2"
-        displayName="My Resonator"
+        displayName="ResonatorDisplayName"
         frequency="10MHz"
         loadCapacitance="10pF"
-        schX={20}
-        schY={15}
+        schX={12}
+        schY={12}
       />
-
-      {/* Row 5 */}
       <solderjumper
         name="SJ1"
-        displayName="My SolderJumper"
-        schX={0}
-        schY={20}
+        displayName="SolderJumperDisplayName"
+        schX={18}
+        schY={12}
       />
-      <switch name="SW2" displayName="My Switch" schX={5} schY={20} />
-      <testpoint name="TP1" displayName="My TestPoint" schX={10} schY={20} />
+      <switch name="SW2" displayName="SwitchDisplayName" schX={24} schY={12} />
+
+      {/* Row 4 - Y=18 */}
+      <testpoint
+        name="TP1"
+        displayName="TestPointDisplayName"
+        schX={0}
+        schY={18}
+      />
       <transistor
         name="Q2"
-        displayName="My Transistor"
+        displayName="TransistorDisplayName"
         type="npn"
-        schX={15}
-        schY={20}
+        schX={6}
+        schY={18}
       />
       <voltagesource
         name="V2"
-        displayName="My VoltageSource"
+        displayName="VoltageSourceDisplayName"
         voltage="5V"
-        schX={20}
-        schY={20}
+        schX={12}
+        schY={18}
       />
     </board>,
   )
