7bd71930ba604b9f04d77c4efdf76f31947a9fda
Abdolsalam Adel Allawlabi <abdaslam13.2.2001@gmail.com>
2026-05-01 15:15:04 +0200

    updated @tscircuit/capacity-autorouter (#2214)

--- tests/repros/repro62-xiao-board-rp2040-pcb-packing.test.tsx
+++ tests/repros/repro62-xiao-board-rp2040-pcb-packing.test.tsx
@@ -12,34 +12,29 @@ test("xiao board rp2040 pcb packing", async () => {
 
   circuit.add(
     <XiaoBoard name="U1" variant="RP2040" withPlatedHoles>
-      <RP2040
-        name="U3"
-        connections={{
-          IOVDD1: ["C12.pin1", "net.V3_3"],
-          IOVDD2: ["C14.pin1", "net.V3_3"],
-          IOVDD3: ["C8.pin1", "net.V3_3"],
-          IOVDD4: ["C13.pin1", "net.V3_3"],
-          IOVDD5: ["C15.pin1", "net.V3_3"],
-          IOVDD6: ["C19.pin1", "net.V3_3"],
+      <group packPlacementStrategy="shortest_connection_along_outline">
+        <RP2040
+          name="U3"
+          pcbX={0}
+          pcbY={0}
+          connections={{
+            IOVDD1: ["C5.pin1", "net.V3_3"],
+            DVDD1: ["C2.pin1", "net.V1_1"],
+            DVDD2: ["C3.pin1", "net.V1_1"],
+            GPIO11: ["C4.pin1"],
 
-          DVDD1: ["C18.1", "net.V1_1"],
-          DVDD2: ["C7.1", "net.V1_1"],
+            USB_VDD: "net.USB_VDD",
+            USB_DM: "net.USB_N",
+            USB_DP: "net.USB_P",
 
-          USB_VDD: "net.USB_VDD",
-          USB_DM: "net.USB_N",
-          USB_DP: "net.USB_P",
-
-          GND: "net.GND",
-        }}
-      />
-      <capacitor name="C5" capacitance="10uF" footprint="cap0402" />
-      <capacitor name="C2" capacitance="100nF" footprint="cap0201" />
-      <capacitor name="C3" capacitance="100nF" footprint="cap0201" />
-      <capacitor name="C7" capacitance="100nF" footprint="cap0201" />
-      <capacitor name="C10" capacitance="100nF" footprint="cap0201" />
-      <capacitor name="C15" capacitance="100nF" footprint="cap0201" />
-      <capacitor name="C18" capacitance="100nF" footprint="cap0201" />
-      <capacitor name="C21" capacitance="100nF" footprint="cap0201" />
+            GND: "net.GND",
+          }}
+        />
+        <capacitor name="C5" capacitance="10uF" footprint="cap0402" />
+        <capacitor name="C2" capacitance="100nF" footprint="cap0201" />
+        <capacitor name="C3" capacitance="100nF" footprint="cap0201" />
+        <capacitor name="C4" capacitance="100nF" footprint="cap0201" />
+      </group>
     </XiaoBoard>,
   )
 
