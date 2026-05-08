892a13237daacac228b74fc375beb9d8be034225
tscircuitbot <tscircuitbot@users.noreply.github.com>
2026-05-08 18:35:35 +0000

    formatbot: Automatically format code

--- tests/repros/repro116-arduino-uno.test.tsx
+++ tests/repros/repro116-arduino-uno.test.tsx
@@ -7,7 +7,10 @@ test("repro116: arduino uno trace and via inflation", async () => {
   const { circuit } = getTestFixture()
 
   const converter = new KicadToCircuitJsonConverter()
-  converter.addFile("tests/repros/assets/arduino-uno.source.kicad_pcb", fs.readFileSync("tests/repros/assets/arduino-uno.source.kicad_pcb", "utf8"))
+  converter.addFile(
+    "tests/repros/assets/arduino-uno.source.kicad_pcb",
+    fs.readFileSync("tests/repros/assets/arduino-uno.source.kicad_pcb", "utf8"),
+  )
   converter.runUntilFinished()
   const arduinoUnoCircuitJson = converter.getOutput()
 
@@ -22,6 +25,8 @@ test("repro116: arduino uno trace and via inflation", async () => {
   const pcbTraces = circuit.db.pcb_trace.list()
   expect(pcbTraces.length).toBeGreaterThan(1)
 
-  expect(arduinoUnoCircuitJson).toMatchPcbSnapshot(`${import.meta.path}-before-inflation`)
+  expect(arduinoUnoCircuitJson).toMatchPcbSnapshot(
+    `${import.meta.path}-before-inflation`,
+  )
   expect(circuit).toMatchPcbSnapshot(`${import.meta.path}-after-inflation`)
 }, 15_000)
