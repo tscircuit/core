655ebd37de32c6e9771f52ef6aacd39aacc5cc9f
Mustafa Mulla <mustafamulla765@gmail.com>
2026-05-27 13:11:17 +0530

    upd

--- lib/utils/obstacles/types.ts
+++ lib/utils/obstacles/types.ts
@@ -1,16 +1,1 @@
-export type Obstacle = {
-  obstacleId?: string
-  componentId?: string
-  // TODO include ovals
-  type: "rect" // NOTE: most datasets do not contain ovals
-  layers: string[]
-  zLayers?: number[]
-  center: { x: number; y: number }
-  width: number
-  height: number
-  ccwRotationDegrees?: number
-  connectedTo: string[]
-  isCopperPour?: boolean
-  netIsAssignable?: boolean
-  offBoardConnectsTo?: string[]
-}
+export type { Obstacle } from "lib/utils/autorouting/SimpleRouteJson"
