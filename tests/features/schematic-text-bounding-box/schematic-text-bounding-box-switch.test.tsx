f636b19571055ed7676ce6bc2a3cc1a6cee3c743
tscircuitbot <tscircuitbot@users.noreply.github.com>
2026-06-30 04:52:29 +0000

    formatbot: Automatically format code

--- tests/features/schematic-text-bounding-box/schematic-text-bounding-box-switch.test.tsx
+++ tests/features/schematic-text-bounding-box/schematic-text-bounding-box-switch.test.tsx
@@ -6,34 +6,10 @@ test("switch schematic text bounding box at all rotations", async () => {
 
   circuit.add(
     <board>
-      <switch
-        name="SW1"
-        type="spst"
-        schX={-6}
-        schY={0}
-        schRotation={0}
-      />
-      <switch
-        name="SW2"
-        type="spdt"
-        schX={-2}
-        schY={0}
-        schRotation={90}
-      />
-      <switch
-        name="SW3"
-        type="dpst"
-        schX={2}
-        schY={0}
-        schRotation={180}
-      />
-      <switch
-        name="SW4"
-        type="dpdt"
-        schX={6}
-        schY={0}
-        schRotation={270}
-      />
+      <switch name="SW1" type="spst" schX={-6} schY={0} schRotation={0} />
+      <switch name="SW2" type="spdt" schX={-2} schY={0} schRotation={90} />
+      <switch name="SW3" type="dpst" schX={2} schY={0} schRotation={180} />
+      <switch name="SW4" type="dpdt" schX={6} schY={0} schRotation={270} />
     </board>,
   )
 
