d64e0d5eb3a30d532b8c9558621fe48cbc671f9b
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2026-07-23 13:32:15 +0530

    Update repro-tps61222-trace-intersection.test.tsx

--- tests/repros/repro-tps61222-trace-intersection.test.tsx
+++ tests/repros/repro-tps61222-trace-intersection.test.tsx
@@ -30,7 +30,7 @@ const TPS61222 = (props: ChipProps) => (
 )
 
 const TPS61222TraceIntersectionRepro = (props: SubcircuitProps) => (
-  <subcircuit {...props} routingDisabled schMaxTraceDistance={20}>
+  <subcircuit {...props} routingDisabled>
     <TPS61222
       name="U2"
       schX={0}
