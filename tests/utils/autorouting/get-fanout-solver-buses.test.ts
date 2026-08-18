09671b3ef8a3387b3e6a100c2741eb9ca17e9170
Severin Ibarluzea <seve700@gmail.com>
2026-08-17 18:18:40 -0700

    Apply bus layer props as fanout constraints (#3274)
    * Apply bus layer preferences to fanout routing
    
    * Enforce bus layer constraints in fanout routing
    
    * Use released fanout solver

---
+++ tests/utils/autorouting/get-fanout-solver-buses.test.ts
@@ -0,0 +1,41 @@
+import { expect, test } from "bun:test"
+import { getFanoutSolverBuses } from "lib/utils/autorouting/FanoutAutorouter"
+
+test("fanout solver buses translate layer preferences into allowedLayers", () => {
+  expect(
+    getFanoutSolverBuses([
+      {
+        busId: "DATA",
+        connectionNames: ["D0"],
+        preferredLayer: "inner2",
+        preferredLayers: ["bottom", "inner2"],
+      },
+      {
+        busId: "CONTROL",
+        connectionNames: ["CS"],
+        allowedLayers: ["top", "inner1"],
+        preferredLayer: "inner2",
+        preferredLayers: ["inner1"],
+      },
+      {
+        busId: "UNCONSTRAINED",
+        connectionNames: ["CLK"],
+      },
+    ]),
+  ).toEqual([
+    {
+      busId: "DATA",
+      connectionNames: ["D0"],
+      allowedLayers: ["inner2", "bottom"],
+    },
+    {
+      busId: "CONTROL",
+      connectionNames: ["CS"],
+      allowedLayers: ["inner1"],
+    },
+    {
+      busId: "UNCONSTRAINED",
+      connectionNames: ["CLK"],
+    },
+  ])
+})
