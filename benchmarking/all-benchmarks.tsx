e9c33d1fcb7a56055144a0914f0fbff657f12f99
mohan-bee <mohn08052006@gmail.com>
2026-08-21 15:16:31 +0530

    up

--- benchmarking/all-benchmarks.tsx
+++ benchmarking/all-benchmarks.tsx
@@ -1,10 +1,8 @@
 import { Benchmark1LedMatrix } from "./benchmarks/benchmark1-led-matrix.tsx"
 import { Benchmark2Rp2040DecouplingCapacitors } from "./benchmarks/benchmark2-rp2040-decoupling-capacitors.tsx"
-import { Benchmark3Rv1106g2CircuitJson } from "./benchmarks/benchmark3-rv1106g2-circuit-json.tsx"
 
 export const BENCHMARKS = {
   "benchmark1-led-matrix": Benchmark1LedMatrix,
   "benchmark2-rp2040-decoupling-capacitors":
     Benchmark2Rp2040DecouplingCapacitors,
-  "benchmark3-rv1106g2-circuit-json": Benchmark3Rv1106g2CircuitJson,
 }
