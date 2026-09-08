27b6c1192551e67e31ceac322d61bd49399e64f1
mohan-bee <mohn08052006@gmail.com>
2026-09-09 01:00:38 +0530

    up

--- tests/repros/repro116-arduino-uno-reroute-utils.tsx
+++ tests/repros/repro116-arduino-uno-reroute-utils.tsx
@@ -1,6 +1,5 @@
 import { expect } from "bun:test"
 import fs from "node:fs"
-import { gzipSync } from "node:zlib"
 import type { CircuitJson, PcbTraceRoutePoint } from "circuit-json"
 import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 import { KicadToCircuitJsonConverter } from "kicad-to-circuit-json"
@@ -348,14 +347,5 @@ export async function expectArduinoUnoRerouteRegion({
     importMetaPath,
     snapshotName,
   })
-  console.log(
-    "ARDUINO_SNAPSHOT",
-    importMetaPath,
-    gzipSync(
-      convertCircuitJsonToPcbSvg(afterRerouteCircuit.getCircuitJson(), {
-        showDebugObjects: true,
-      }),
-    ).toString("base64"),
-  )
   expect(afterRerouteCircuit).toMatchPcbSnapshot(importMetaPath)
 }
