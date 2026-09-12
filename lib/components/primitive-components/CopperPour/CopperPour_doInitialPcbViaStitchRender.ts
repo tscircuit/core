da46215ec00eca00b8bf950b0e060e285fedc4c4
imrishabh18 <belikerishabh@gmail.com>
2026-09-13 00:47:04 +0530

    Pass viaStitchPitch directly to the stitching solver

--- lib/components/primitive-components/CopperPour/CopperPour_doInitialPcbViaStitchRender.ts
+++ lib/components/primitive-components/CopperPour/CopperPour_doInitialPcbViaStitchRender.ts
@@ -36,7 +36,7 @@ const renderViaStitchingForCopperPours = (copperPour: CopperPour) => {
       sourceNetIds: [...new Set(sourceNetIds)],
       viaHoleDiameter: pcbBoard?.min_via_hole_diameter ?? holeDiameter,
       viaOuterDiameter: pcbBoard?.min_via_pad_diameter ?? padDiameter,
-      viaPitch: boardComponent?._parsedProps.viaStitchPitch,
+      viaStitchPitch: boardComponent?._parsedProps.viaStitchPitch,
     },
   }
   const solver = new ViaStitchSolver(solverInput)
