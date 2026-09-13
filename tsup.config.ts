ecb6c441482510523f7f2bfddf80e1221a2b6118
seveibar <seveibar@gmail.com>
2026-09-13 15:35:01 -0700

    Install solver package for exported preflight event types

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   entry: ["index.ts"],
   format: ["esm"],
-  dts: { resolve: ["@tscircuit/preflight-routing-solver"] },
+  dts: true,
   noExternal: [
     "@tscircuit/preflight-routing-solver",
     "@tscircuit/fanout-solver",
