84d01cb6bde5b2642f040b288568a88bf9a043f5
seveibar <seveibar@gmail.com>
2026-09-19 22:18:54 -0700

    test: fully specify OLED geometry in minimal screen repro

--- tests/repros/repro-assembly-screen.test.tsx
+++ tests/repros/repro-assembly-screen.test.tsx
@@ -10,6 +10,7 @@ test("assembly.screen attaches an OLED to a PCB connector", async () => {
       <board name="B1" width="44mm" height="36mm" routingDisabled>
         <connector
           name="J1"
+          pcbX={0}
           pcbY={-13}
           pinCount={30}
           footprint="fpc30_p0.5mm_pw0.3mm_pl1.25mm_mpx17.58mm_mpy2.325mm_mpw2mm_mpl3mm_mounttop"
@@ -18,7 +19,7 @@ test("assembly.screen attaches an OLED to a PCB connector", async () => {
       <assembly.screen
         name="SCREEN"
         connectsTo=".B1 .J1"
-        cadModel="flexscreen_w26.7mm_h19.26mm_flex12mm_flexwidth15.5mm_conductors30_conductorpitch0.5mm_conductorwidth0.3mm_edgemargin0.35mm_sitsflat"
+        cadModel="flexscreen_w26.7mm_h19.26mm_screenthickness1.45mm_bezelinset1mm_bezeldepth0.5mm_activew21.744mm_activeh10.864mm_flex12mm_flexwidth15.5mm_flexthickness0.3mm_conductors30_conductorpitch0.5mm_conductorwidth0.3mm_edgemargin0.35mm_contactlength4mm_stiffenerlength4.5mm_stiffenerthickness0.12mm_sitsflat_cablestarty4.285mm_cablestartz1.1mm_hideconductors_screencolor(#071c18)_bezelcolor(#171a1d)"
       />
     </assembly.device>,
   )
@@ -39,7 +40,7 @@ test("assembly.screen attaches an OLED to a PCB connector", async () => {
   ).toStartWith("flexscreen_")
 
   await expect(circuit).toMatchSimple3dSnapshot(import.meta.path, {
-    camPos: [45, 38, 55],
+    camPos: [-45, 38, -55],
     poppygl: {
       lookAt: [0, 0, 0],
       backgroundColor: [1, 1, 1],
